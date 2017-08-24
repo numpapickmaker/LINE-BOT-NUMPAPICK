84c87b4f2678584bc3c3cb0535f0c822d232f584
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-08-24 21:37:59 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,39 +1,6 @@
 <?php
  require("pub.php");
  require("line.php");
-$access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
-
- if( !is_null($_GET['name'])){
-			$messages = [
-				'type' => 'text',
-				'text' => $_GET['name']
-				//'text' => $text
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/push';
-			$data = [
-
-				'to' => 'Ue77a191627f6ac91899e75d92264310c',
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-	
-echo "OK";
-}
-
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -41,39 +8,11 @@ $content = file_get_contents('php://input');
 
 $events = json_decode($content, true);
 // Validate parsed JSON data
-if (!is_null($events['sensorType'])) {
+if (!is_null($events['ESP'])) {
 	
-	send_LINE($events['sensorType']);
-		/*$messages = [
-				'type' => 'text',
-				'text' => $events['sensorType']
-				//'text' => $text
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/push';
-			$data = [
-
-				'to' => 'Ue77a191627f6ac91899e75d92264310c',
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n"; */
-		//}
-	//}
-//}
-echo "OK";
+	send_LINE($events['ESP']);
+		
+	echo "OK";
 	}
 if (!is_null($events['events'])) {
 	echo "line bot";
