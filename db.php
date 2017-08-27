eef7683e62978a0e6aedc0680ce2c2b054707ee0
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-08-28 02:41:56 +0700

    Update db.php

--- db.php
+++ db.php
@@ -2,41 +2,33 @@
  
    
    function save_userid($userid,$esp){ 
-   $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
-   $port        = "port=5432";
-   $dbname      = "dbname=ddagopqfb1uood";
-   $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
+      $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
+      $port        = "port=5432";
+      $dbname      = "dbname=ddagopqfb1uood";
+      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
 
-   $db = pg_connect( "$host $port $dbname $credentials"  ) ;
-   if(!$db) {
-      echo "Error : Unable to open database\n";
-   } else {
-      echo "Opened database successfully\n";
-   }
-   $sql ="SELECT MAX(userno) as LargestNO from userline;";
-
-
- $ret = pg_query($db, $sql) ;
-   if(!$ret) {
-      echo pg_last_error($db) ;
-   } else {
-      while($row = pg_fetch_row($ret) ){
-      echo "NO max = " . $row[0] . "\n";
-      $row[0] = intval($row[0]+1);
-       $sql =" INSERT INTO userline (userno,id,esp) VALUES ( ".$row[0].",'".$userid."','".$esp."');";
+      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
+      if(!$db) {
+         echo "Error : Unable to open database\n";
+      } else {
+         echo "Opened database successfully\n";
       }
-      //echo "Records created successfully\n";
-   }
+      $sql ="SELECT MAX(userno) as LargestNO from userline;";
 
-  
 
-   $ret = pg_query($db, $sql) ;
-   if(!$ret) {
-      echo pg_last_error($db) ;
-   } else {
-      echo "Records created successfully\n";
-   }
-   pg_close($db) ;
+    $ret = pg_query($db, $sql) ;
+      if(!$ret) {
+         echo pg_last_error($db) ;
+      } else {
+         while($row = pg_fetch_row($ret) ){
+         echo "NO max = " . $row[0] . "\n";
+         $row[0] = intval($row[0]+1);
+          $sql =" INSERT INTO userline (userno,id,esp) VALUES ( ".$row[0].",'".$userid."','".$esp."');";
+         }
+         //echo "Records created successfully\n";
+      }
+
+      pg_close($db) ;
    }
    function check_userid($userid){
       $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
