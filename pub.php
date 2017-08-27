d9b8cf093b45e6d092ae08877d5245d1ea5c2574
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-08-28 03:33:04 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -1,9 +1,9 @@
  <?php
  function pubMqtt($topic,$msg){
-       $APPID= "numpapicklinebot"; //enter your appid
+       $APPID= "numpapicklinebot/"; //enter your appid
      $KEY = "mJ7K4MfteC7p0dW"; //enter your key
     $SECRET = "pp4gzMhCvJIqlxc66hKEvk46m"; //enter your secret
-    $Topic = "/NodeMCU1"; 
+    $Topic = "$topic"; 
       put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
  
   }
