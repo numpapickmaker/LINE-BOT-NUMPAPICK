ef732915126a86f81c70d3dcd0f2c2e38b0189ee
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-08-28 21:23:12 +0700

    Update db.php

--- db.php
+++ db.php
@@ -119,7 +119,7 @@
             //check_send($row[1],$msg);
           $checking = 1 ;
           
-           check_userlogin($userid,$row[1]);
+          // check_userlogin($userid,$row[1]);
              
          }
          if($checking == 0){
