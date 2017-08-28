38339f772e2aad010f011f6a9bd67f2585f79b8e
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-08-28 19:00:17 +0700

    Update db.php

--- db.php
+++ db.php
@@ -58,7 +58,6 @@
                         send_LINE("checking",$userid);
                         getMqttfromlineMsg($row[2],$msg);
                 }  
-
                   
         if( $checking == 0){
              send_LINE("Please login",$userid);
@@ -105,5 +104,73 @@
      
       pg_close($db) ;
    }
-  
+   function check_esp($esp){
+      $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
+      $port        = "port=5432";
+      $dbname      = "dbname=ddagopqfb1uood";
+      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
+      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
+      if(!$db) {
+         echo "Error : Unable to open database\n";
+      } else {
+         echo "Opened database successfully\n";
+      }
+      $sql ="SELECT * FROM device WHERE espname='".$esp."';";
+    $ret = pg_query($db, $sql) ;
+      if(!$ret) {
+         echo pg_last_error($db) ;
+      } else {
+         $checking = 0;
+         while($row = pg_fetch_row($ret) ){
+          echo "have espname = " . $row[1] . "\n";
+         // send_LINE('PASS');
+            $checking = 1 ;
+            
+             
+         }
+         if($checking == 0){
+               save_esp($esp);
+         }
+         //echo "Records created successfully\n";
+      }
+     
+      pg_close($db) ;
+   }
+    function save_esp($esp){ 
+      $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
+      $port        = "port=5432";
+      $dbname      = "dbname=ddagopqfb1uood";
+      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
+      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
+      if(!$db) {
+         echo "Error : Unable to open database\n";
+      } else {
+         echo "Opened database successfully\n";
+      }
+      $sql ="SELECT MAX(espno) as LargestNO from device;";
+    $ret = pg_query($db, $sql) ;
+      if(!$ret) {
+         echo pg_last_error($db) ;
+      } else {
+         while($row = pg_fetch_row($ret) ){
+         echo "NO max = " . $row[0] . "\n";
+         $row[0] = intval($row[0]+1);
+          $sql =" INSERT INTO device (espno,espname) VALUES ( ".$row[0].",'".$esp."');";
+         }
+         //echo "Records created successfully\n";
+      }
+    $ret = pg_query($db, $sql) ;
+      if(!$ret) {
+         echo pg_last_error($db) ;
+      } else {
+         
+        echo "Records created successfully\n";
+         //getMqttfromlineMsg("555");
+                
+         
+      }
+      pg_close($db) ;
+   }
+   $esp = "NodeMCU1";
+   check_esp($esp;  
 ?>
