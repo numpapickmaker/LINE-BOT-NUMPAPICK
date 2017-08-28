515e783168707a976a7ae9fedb3adcec59360b72
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-08-28 19:03:54 +0700

    Update db.php

--- db.php
+++ db.php
@@ -139,38 +139,7 @@ function save_esp($esp){
       }
       pg_close($db) ;
    }
-   function check_esp($esp){
-      $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
-      $port        = "port=5432";
-      $dbname      = "dbname=ddagopqfb1uood";
-      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
-      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
-      if(!$db) {
-         echo "Error : Unable to open database\n";
-      } else {
-         echo "Opened database successfully\n";
-      }
-      $sql ="SELECT * FROM device WHERE espname='".$esp."';";
-    $ret = pg_query($db, $sql) ;
-      if(!$ret) {
-         echo pg_last_error($db) ;
-      } else {
-         $checking = 0;
-         while($row = pg_fetch_row($ret) ){
-          echo "have espname = " . $row[1] . "\n";
-         // send_LINE('PASS');
-            $checking = 1 ;
-            
-             
-         }
-         if($checking == 0){
-               save_esp($esp);
-         }
-         //echo "Records created successfully\n";
-      }
-     
-      pg_close($db) ;
-   }
+
     
    $esp = "NodeMCU1";
    check_esp($esp;  
