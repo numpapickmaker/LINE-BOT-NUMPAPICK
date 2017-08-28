d3a220aedd4c09c271d8736d6c58922db9f1afb3
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-08-28 21:20:55 +0700

    Update db.php

--- db.php
+++ db.php
@@ -68,23 +68,33 @@
       pg_close($db) ;
    }
    function check_userlogin($userid,$esp){
-      /* $sql ="SELECT * FROM userline WHERE id='".$userid."';";
-    $ret = pg_query($db, $sql) ;
-      if(!$ret) {
-         echo pg_last_error($db) ;
+       $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
+      $port        = "port=5432";
+      $dbname      = "dbname=ddagopqfb1uood";
+      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
+      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
+      if(!$db) {
+         echo "Error : Unable to open database\n";
       } else {
-         $checking = 0;   
-              while($row = pg_fetch_row($ret)){
-                  echo "ESP name = " . $row[2] . "\n";
-                  // send_LINE('PASS')
-                   $checking = 1;     
-                        send_LINE("you login already",$userid);
-                        
-                }  
-                  
-        if( $checking == 0){
-             save_userid($userid,$esp);
-        }*/
+         echo "Opened database successfully\n";
+      }
+       $sql ="SELECT * FROM userline WHERE id='".$userid."' AND esp='".$esp."';";
+       $ret = pg_query($db, $sql) ;
+         if(!$ret) {
+            echo pg_last_error($db) ;
+         } else {
+            $checking = 0;   
+                 while($row = pg_fetch_row($ret)){
+                     echo "ESP name = " . $row[2] . "\n";
+                     // send_LINE('PASS')
+                      $checking = 1;     
+                      send_LINE("you login already",$userid);
+                           
+                   }  
+                     
+           if( $checking == 0){
+                save_userid($userid,$esp);
+           }
    }
    function check_login($userid,$esp){
       $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
