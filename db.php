3889cb1827e34f66d9d533f03f1f3c1ff88e3222
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-08-28 20:56:50 +0700

    Update db.php

--- db.php
+++ db.php
@@ -34,7 +34,7 @@
       }
       pg_close($db) ;
    }
-   function check_userid($userid,$msg){
+   function check_userid($userid,$esp){
       $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
       $port        = "port=5432";
       $dbname      = "dbname=ddagopqfb1uood";
@@ -66,6 +66,38 @@
       }
      
       pg_close($db) ;
+   }
+   function check_login($userid,$esp){
+      $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
+      $port        = "port=5432";
+      $dbname      = "dbname=ddagopqfb1uood";
+      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
+      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
+      if(!$db) {
+         echo "Error : Unable to open database\n";
+      } else {
+         echo "Opened database successfully\n";
+      }
+     $sql ="SELECT * FROM device WHERE espname='".$esp."';";
+    $ret = pg_query($db, $sql) ;
+      if(!$ret) {
+         echo pg_last_error($db) ;
+      } else {
+         $checking = 0;
+         while($row = pg_fetch_row($ret) ){
+          echo "have espname = " . $row[1] . "\n";
+          // send_LINE('PASS');
+            //check_send($row[1],$msg);
+          $checking = 1 ;
+          save_userid($userid,$row[1]);
+            
+             
+         }
+         if($checking == 0){
+            send_LINE("Incorrect Device Name",$userid);
+         }
+         //echo "Records created successfully\n";
+      }
    }
     function check_send($esp,$msg){
       $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
