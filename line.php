2019a4a7f8e85cd6b3958bc859fca590e46e3a2d
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-08-28 12:25:12 +0700

    Update line.php

--- line.php
+++ line.php
@@ -29,7 +29,7 @@ function send_LINE($msg,$userid){
       echo $result . "\r\n"; 
 }
 
-function send_Check(){
+function send_Check($userid){
    $access_token = '3NZ4tPcC9W1t6cPI0r3ezvnsoK8KW04hbCSPxKSahSeGbeUU7lC8PQvx02uN5UyL7wOaVJ6EZ9oM5uQjkqLDNZtagQuRcS/NaaGmtopk7pBGOXtNk3lDc4KQIns5tV/jpm8yyr/114JL4uORE5czWwdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
@@ -58,7 +58,7 @@ function send_Check(){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => $userid,
         'messages' => [$messages],
       ];
       $post = json_encode($data);
