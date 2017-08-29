aac43b177e1047f87444614ff22c4b5d0667f8da
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-08-29 14:48:23 +0700

    Update line.php

--- line.php
+++ line.php
@@ -142,26 +142,10 @@ function send_PRESS($userid){
 function send_LOWBAT($userid){
    $access_token = '3NZ4tPcC9W1t6cPI0r3ezvnsoK8KW04hbCSPxKSahSeGbeUU7lC8PQvx02uN5UyL7wOaVJ6EZ9oM5uQjkqLDNZtagQuRcS/NaaGmtopk7pBGOXtNk3lDc4KQIns5tV/jpm8yyr/114JL4uORE5czWwdB04t89/1O/w1cDnyilFU='; 
   $messages = [
-          "type"=> "template",
-          "altText"=> "this is a buttons template",
-          "template"=> [
-              "type"=> "buttons",
-              "thumbnailImageUrl"=> "https://i.imgur.com/c65ILrh.jpg",
-              "title"=> "Menu",
-              "text"=> "Please select",
-              "actions"=> [
-                  [
-                    "type"=> "message",
-                    "label"=> "Acknowledge",
-                    "text"=> "Acknowledge"
-                  ],
-                  [
-                    "type"=> "uri",
-                    "label"=> "View detail",
-                    "uri"=> "http://app.midatdb.com/PFrCbTJd2J"
-                  ]
-              ]
-          ]
+         
+                "type"=> "image",
+                "originalContentUrl"=> "https://i.imgur.com/c65ILrh.jpg",
+                "previewImageUrl"=> "https://i.imgur.com/c65ILrh.jpg"
       ];
 
       // Make a POST Request to Messaging API to reply to sender
