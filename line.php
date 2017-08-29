8bbb69ca4dad236a4e277177fb7e931995a6ee48
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-08-29 13:27:45 +0700

    Update line.php

--- line.php
+++ line.php
@@ -6,17 +6,14 @@ function send_LINE($msg,$userid){
         'text' => $msg
         //'text' => $text
       ]; 
-
       // Make a POST Request to Messaging API to reply to sender
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
-
         'to' => $userid,
         'messages' => [$messages],
       ];
       $post = json_encode($data);
       $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
       $ch = curl_init($url);
       curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -25,19 +22,16 @@ function send_LINE($msg,$userid){
       curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
       $result = curl_exec($ch);
       curl_close($ch);
-
       echo $result . "\r\n"; 
 }
-
 function send_Check($userid,$img){
    $access_token = '3NZ4tPcC9W1t6cPI0r3ezvnsoK8KW04hbCSPxKSahSeGbeUU7lC8PQvx02uN5UyL7wOaVJ6EZ9oM5uQjkqLDNZtagQuRcS/NaaGmtopk7pBGOXtNk3lDc4KQIns5tV/jpm8yyr/114JL4uORE5czWwdB04t89/1O/w1cDnyilFU='; 
-
   $messages = [
           "type"=> "template",
           "altText"=> "this is a buttons template",
           "template"=> [
               "type"=> "buttons",
-              "thumbnailImageUrl"=>"http://i.imgur.com/yLAJlP5.jpg",
+              "thumbnailImageUrl"=> $img,
               "title"=> "EMERGENCY ALERT !!",
               "text"=> "Please check user",
               "actions"=> [
@@ -57,13 +51,11 @@ function send_Check($userid,$img){
       // Make a POST Request to Messaging API to reply to sender
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
-
         'to' => $userid,
         'messages' => [$messages],
       ];
       $post = json_encode($data);
       $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
       $ch = curl_init($url);
       curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -72,8 +64,6 @@ function send_Check($userid,$img){
       curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
       $result = curl_exec($ch);
       curl_close($ch);
-
       echo $result . "\r\n"; 
 }
 ?>
-
