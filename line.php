b083c28d9e64c286b6b94f54bc398e5f5fca174e
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-08-29 14:05:06 +0700

    Update line.php

--- line.php
+++ line.php
@@ -24,7 +24,7 @@ function send_LINE($msg,$userid){
       curl_close($ch);
       echo $result . "\r\n"; 
 }
-function send_Check($userid){
+function send_CHECK($userid){
    $access_token = '3NZ4tPcC9W1t6cPI0r3ezvnsoK8KW04hbCSPxKSahSeGbeUU7lC8PQvx02uN5UyL7wOaVJ6EZ9oM5uQjkqLDNZtagQuRcS/NaaGmtopk7pBGOXtNk3lDc4KQIns5tV/jpm8yyr/114JL4uORE5czWwdB04t89/1O/w1cDnyilFU='; 
   $messages = [
           "type"=> "template",
