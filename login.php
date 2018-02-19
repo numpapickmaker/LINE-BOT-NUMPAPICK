b21010b577c4ef61e4e087fa6fb6ba6f54949620
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-02-20 01:48:35 +0700

    Update login.php

--- login.php
+++ login.php
@@ -22,7 +22,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     } else{
         $password = trim($_POST['password']);
     }
-    
+    check_loginpage($username,$password);
     /*// Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
@@ -72,6 +72,38 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Close connection
     mysqli_close($link);*/
 }
+function check_loginpage($username,$password){
+      $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
+      $port        = "port=5432";
+      $dbname      = "dbname=ddagopqfb1uood";
+      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
+      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
+      if(!$db) {
+         echo "Error : Unable to open database\n";
+      } else {
+         echo "Opened database successfully\n";
+      }
+      $sql ="SELECT * FROM device WHERE espname='".$username."';";
+    $ret = pg_query($db, $sql) ;
+      if(!$ret) {
+         echo pg_last_error($db) ;
+      } else {
+         $checking = 0;
+         while($row = pg_fetch_row($ret) ){
+          echo "have espname = " . $row[1] . "\n";
+          // send_LINE('PASS');
+            header("location: bot.php");
+            
+             
+         }
+         if($checking == 0){
+             $username_err = 'No account found with that username.';
+         }
+         //echo "Records created successfully\n";
+      }
+     
+      pg_close($db) ;
+   }
 ?>
  
 <!DOCTYPE html>
