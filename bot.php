3c1f5894a5527c34824dabe828ce03d0a07f8315
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-02-27 01:38:18 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -32,7 +32,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'follow') {
 			$userId = $event['source']['userId'];
-			send_Menu('welcome to smarthelper ',$userId);
+			send_Menu($userId);
 		}
 		else if ($event['type'] == 'postback') {
 			$userId = $event['source']['userId'];
