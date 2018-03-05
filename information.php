61c2cb4f1893b1601ca4c2171d204dbd54d715a6
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-03-06 02:35:29 +0700

    Update information.php

--- information.php
+++ information.php
@@ -1,7 +1,7 @@
 
  <?php
 if( $_GET["name"]|| $_GET["age"]|| $_GET["sex"]|| $_GET["heigth"]|| $_GET["weigth"]|| $_GET["disease"]|| $_GET["address"]|| $_GET["Phone"]){
-  echo  $_GET["name"]
+  echo  $_GET["name"];
 }
 echo $_GET["view"];
   $test = "Hello";
