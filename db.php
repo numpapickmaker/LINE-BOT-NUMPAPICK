44eba48c20d1943a58ccd528d7c3dbb29eabe0ac
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-03-06 08:46:32 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,85 @@
-<?php
-    if($_GET["userid"]){
-      $userid = $_GET["userid"];
-      $esp = $_GET["esp"];
-      check_userlogout($userid,$esp);
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        #customers {
+        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+        width: 75%;
+        
     }
-   function save_userid($userid,$esp){ 
-      $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
+
+    #customers td, #customers th {
+        border: 1px solid #ddd;
+        padding: 8px;
+    }
+
+    #customers tr:nth-child(even){background-color: #f2f2f2;}
+
+    #customers tr:hover {background-color: #ddd;}
+
+    #customers th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #4CAF50;
+        color: white;
+    }
+    .button {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 10px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    
+    margin: 4px 2px;
+    cursor: pointer;
+}
+
+.button1 {border-radius: 8px;}
+.button2 {border-radius: 8px; background-color: #f44336;}
+.button3 { position: relative;
+  
+  left: 90%;border-radius: 8px; background-color: #008CBA;}
+
+    </style>
+</head>
+<body>
+
+<script>
+function del_device(a, b) {
+    var txt;
+    var r = confirm("You want to delete device?" + a);
+    if (r == true) {
+    	window.location.href = "https://numpapick.herokuapp.com/db.php?userid="+a+"&esp="+b;
+    } else {
+        txt = "You pressed Cancel!";
+    }
+    
+}
+</script>
+<form action="https://numpapick.herokuapp.com/login.php" method="get">
+    <button class="button button3" value="<?php echo $_GET["action"];?>" name="add">+</button>
+</form>
+
+<table  id="customers" align="center">
+  <tr>
+    <th > <div align="center">No </div></th>
+    <th > <div align="center">Device id </div></th>
+    <th > <div align="center">Menu</div></th>
+  </tr>
+
+<?php
+echo $_GET["action"];
+  $test = "Hello";
+  $No =$Device_id =$Name =$Age =$Sex =$Heigth =$Weigth =$disease =$address = $phone = "";
+ $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
       $port        = "port=5432";
       $dbname      = "dbname=ddagopqfb1uood";
       $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
@@ -13,149 +87,9 @@
       if(!$db) {
          echo "Error : Unable to open database\n";
       } else {
-         echo "Opened database successfully\n";
+         //echo "Opened database successfully\n";
       }
-      $sql ="SELECT MAX(userno) as LargestNO from userline;";
-    $ret = pg_query($db, $sql) ;
-      if(!$ret) {
-         echo pg_last_error($db) ;
-      } else {
-         while($row = pg_fetch_row($ret) ){
-         echo "NO max = " . $row[0] . "\n";
-         $row[0] = intval($row[0]+1);
-          $sql =" INSERT INTO userline (userno,id,esp) VALUES ( ".$row[0].",'".$userid."','".$esp."');";
-         }
-         //echo "Records created successfully\n";
-      }
-    $ret = pg_query($db, $sql) ;
-      if(!$ret) {
-       send_LINE("Login Error!",$userid);
-         echo pg_last_error($db) ;
-      } else {
-       send_LINE("Login success",$userid);
-        echo "Records created successfully\n";
-         //getMqttfromlineMsg("555");
-                
-         
-      }
-      pg_close($db) ;
-   }
-   function check_userid($userid,$msg){
-      $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
-      $port        = "port=5432";
-      $dbname      = "dbname=ddagopqfb1uood";
-      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
-      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
-      if(!$db) {
-         echo "Error : Unable to open database\n";
-      } else {
-         echo "Opened database successfully\n";
-      }
-      $sql ="SELECT * FROM userline WHERE id='".$userid."';";
-    $ret = pg_query($db, $sql) ;
-      if(!$ret) {
-         echo pg_last_error($db) ;
-      } else {
-         $checking = 0;   
-              while($row = pg_fetch_row($ret)){
-                  echo "ESP name = " . $row[2] . "\n";
-                  // send_LINE('PASS')
-                   $checking = 1;     
-                        send_LINE("Wait a Second",$userid);
-                        getMqttfromlineMsg($row[2],$msg);
-                }  
-                  
-        if( $checking == 0){
-             send_LINE("Please login",$userid);
-        }
-         //echo "Records created successfully\n";
-      }
-     
-      pg_close($db) ;
-   }
-   function check_userlogin($userid,$esp){
-       $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
-      $port        = "port=5432";
-      $dbname      = "dbname=ddagopqfb1uood";
-      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
-      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
-      if(!$db) {
-         echo "Error : Unable to open database\n";
-      } else {
-         echo "Opened database successfully\n";
-      }
-       $sql ="SELECT * FROM userline WHERE id='".$userid."' AND esp='".$esp."';";
-       $ret = pg_query($db, $sql) ;
-         if(!$ret) {
-            echo pg_last_error($db) ;
-         } else {
-            $checking = 0;   
-                 while($row = pg_fetch_row($ret)){
-                     echo "ESP name = " . $row[2] . "\n";
-                     // send_LINE('PASS')
-                      $checking = 1;     
-                      send_LINE("you login already",$userid);
-                           
-                   }  
-                  
-           if( $checking == 0){
-                save_userid($userid,$esp);
-           }
-        }
-   }
-   function check_userlogout($userid,$esp){
-       $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
-      $port        = "port=5432";
-      $dbname      = "dbname=ddagopqfb1uood";
-      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
-      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
-      if(!$db) {
-         echo "Error : Unable to open database\n";
-      } else {
-         echo "Opened database successfully\n";
-      }
-       $sql ="SELECT * FROM userline WHERE id='".$userid."' AND esp='".$esp."';";
-       $ret = pg_query($db, $sql) ;
-         if(!$ret) {
-            echo pg_last_error($db) ;
-         } else {
-            $checking = 0;   
-                 while($row = pg_fetch_row($ret)){
-                     //echo "ESP name = " . $row[2] . "\n";
-                     // send_LINE('PASS')
-                      $checking = 1;     
-                   //   send_LINE("you login already",$userid); 
-                      $sql ="DELETE FROM userline WHERE id='".$userid."' AND esp='".$esp."';";
-                      $ret = pg_query($db, $sql) ;
-                      if(!$ret) {
-                        send_LINE("Logout Error!",$userid);
-                         echo pg_last_error($db) ;
-                      } else {
-                            
-                          send_LINE("logout success",$userid);
-                                                 
-                                }     
-                  }  
-                    
-          if( $checking == 0){
-                //save_userid($userid,$esp);
-              send_LINE("you not login",$userid);
-          }
-        }
-        pg_close($db) ;
-   }
-   function check_login($userid,$esp){
-      $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
-      $port        = "port=5432";
-      $dbname      = "dbname=ddagopqfb1uood";
-      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
-      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
-      if(!$db) {
-         echo "Error : Unable to open database\n";
-      } else {
-         echo "Opened database successfully\n";
-      }
-     $sql ="SELECT * FROM device WHERE espname='".$esp."';";
+     $sql ="SELECT * FROM userline WHERE id='".$_GET["action"]."';";
     $ret = pg_query($db, $sql) ;
       if(!$ret) {
          echo pg_last_error($db) ;
@@ -164,129 +98,36 @@
          while($row = pg_fetch_row($ret) ){
           echo "have espname = " . $row[1] . "\n";
           // send_LINE('PASS');
-            //check_send($row[1],$msg);
-          $checking = 1 ;
+          //  header("location: bot.php");
+           $No = $row[0];
+           $Device_id = $row[2];
           
-           check_userlogin($userid,$row[1]);
-             
+           ?>
+           <tr>
+    <td><div align="center"><?php echo $No;?></div></td>
+    <td><?php echo $Device_id;?></td>
+    
+    <td align="center">
+    <form action="https://numpapick.herokuapp.com/information.php" method="get">
+    <button class="button button1" value="<?php echo $Device_id;?>" name="view">View</button>
+    </form>
+<button class="button button2" onclick="del_device(<?php echo $Device_id;?>,<?php echo $_GET["action"]) ?>">Delete</button>
+</td>
+  </tr>
+  <?php
          }
          if($checking == 0){
-            send_LINE("Incorrect Device Name",$userid);
-         }
-         //echo "Records created successfully\n";
-      }
-   }
-    function check_send($esp,$msg){
-      $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
-      $port        = "port=5432";
-      $dbname      = "dbname=ddagopqfb1uood";
-      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
-      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
-      if(!$db) {
-         echo "Error : Unable to open database\n";
-      } else {
-         echo "Opened database successfully\n";
-      }
-      $sql ="SELECT * FROM userline WHERE esp='".$esp."';";
-    $ret = pg_query($db, $sql) ;
-      if(!$ret) {
-         echo pg_last_error($db) ;
-      } else {
-         while($row = pg_fetch_row($ret) ){
-         echo "userid = " . $row[1] . "\n";
-         // send_LINE('PASS');
-            
-            if($row[1] == " "){
-               //send_LINE('Please Login',$userid);
-             
-            }else{
-     
-               if($msg == "FALL"){
-                   send_FALL($row[1]);
-               }else if($msg == "CHECK"){
-                 send_CHECK($row[1]); 
-               }else if($msg == "LOW"){
-                 send_LOWBAT($row[1]); 
-               }else if($msg == "PRESS"){
-                 send_PRESS($row[1]); 
-               }else{
-                  send_LINE($msg,$row[1]);
-               }
-            }  
+            // $username_err = 'No account found with that username.';
          }
          //echo "Records created successfully\n";
       }
      
-      pg_close($db) ;
-   }
-function save_esp($esp){ 
-      $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
-      $port        = "port=5432";
-      $dbname      = "dbname=ddagopqfb1uood";
-      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
-      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
-      if(!$db) {
-         echo "Error : Unable to open database\n";
-      } else {
-         echo "Opened database successfully\n";
-      }
-      $sql ="SELECT MAX(espno) as LargestNO from device;";
-    $ret = pg_query($db, $sql) ;
-      if(!$ret) {
-         echo pg_last_error($db) ;
-      } else {
-         while($row = pg_fetch_row($ret) ){
-         echo "NO max = " . $row[0] . "\n";
-         $row[0] = intval($row[0]+1);
-          $sql =" INSERT INTO device (espno,espname) VALUES ( ".$row[0].",'".$esp."');";
-         }
-         //echo "Records created successfully\n";
-      }
-    $ret = pg_query($db, $sql) ;
-      if(!$ret) {
-         echo pg_last_error($db) ;
-      } else {
-         
-        echo "Records created successfully\n";
-         //getMqttfromlineMsg("555");
-                
-         
-      }
-      pg_close($db) ;
-   }
-   function check_esp($esp,$msg){
-      $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
-      $port        = "port=5432";
-      $dbname      = "dbname=ddagopqfb1uood";
-      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
-      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
-      if(!$db) {
-         echo "Error : Unable to open database\n";
-      } else {
-         echo "Opened database successfully\n";
-      }
-      $sql ="SELECT * FROM device WHERE espname='".$esp."';";
-    $ret = pg_query($db, $sql) ;
-      if(!$ret) {
-         echo pg_last_error($db) ;
-      } else {
-         $checking = 0;
-         while($row = pg_fetch_row($ret) ){
-          echo "have espname = " . $row[1] . "\n";
-          // send_LINE('PASS');
-            check_send($row[1],$msg);
-            $checking = 1 ;
-            
-             
-         }
-         if($checking == 0){
-             save_esp($esp);
-         }
-         //echo "Records created successfully\n";
-      }
-     
-      pg_close($db) ;
-   }
-    
-    
+      pg_close($db) ;   
+      
 ?>
+
+  
+</table>
+
+</body>
+</html>
