092016d659e198ad97a9f75c123396f040bda3da
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-03-06 09:01:41 +0700

    Update manage.php

--- manage.php
+++ manage.php
@@ -55,7 +55,7 @@
 <script>
 function del_device(a, b) {
     var txt;
-    var r = confirm("You want to delete "+a" ?" );
+    var r = confirm("You want to delete device?"+a );
     if (r == true) {
     	window.location.href = "https://numpapick.herokuapp.com/db.php?userid="+a+"&esp="+b;
     } else {
@@ -76,8 +76,8 @@ function del_device(a, b) {
   </tr>
 
 <?php
-//echo $_GET["action"];
-  $esp = $_GET["action"];
+echo $_GET["action"];
+  $test = "Hello";
   $No =$Device_id =$Name =$Age =$Sex =$Heigth =$Weigth =$disease =$address = $phone = "";
  $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
       $port        = "port=5432";
@@ -111,7 +111,7 @@ function del_device(a, b) {
     <form action="https://numpapick.herokuapp.com/information.php" method="get">
     <button class="button button1" value="<?php echo $Device_id;?>" name="view">View</button>
     </form>
-<button class="button button2" onclick="del_device('<?php echo $Device_id;?>','<?php echo $_GET["action"];?>')">Delete</button>
+<button class="button button2" onclick="del_device('<?php echo $Device_id;?>',1)">Delete</button>
 </td>
   </tr>
   <?php
