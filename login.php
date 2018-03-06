7603ca759b58c617b2c6e59393ea927e9586637d
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-03-06 07:44:46 +0700

    Update login.php

--- login.php
+++ login.php
@@ -100,7 +100,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <body>
     <div class="wrapper">
         <h2>Login</h2>
-        <p>Please fill in your credentials to login.</p>
+        <p>Please fill in your credentials to login. <?php echo $userid; ?></p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>Username</label>
