f9a0b67f2faddd80017589e41b153efbcffe013c
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-03-12 04:36:34 +0700

    Update information.php

--- information.php
+++ information.php
@@ -166,7 +166,7 @@ function Edit() {
 .button2 { position: relative; 
  left: 3%;border-radius: 8px; background-color: #f44336;}   
 .button4 { position: relative; 
- left: 50%;border-radius: 8px; background-color: #f44336;} 
+ left: 40%;border-radius: 8px; background-color: #f44336;} 
     </style>
 </head>
 <body>
