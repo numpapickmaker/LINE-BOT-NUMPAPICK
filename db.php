0a8bbd53f6e3012689a5e887064d015e7f9decaa
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-03-11 23:21:59 +0700

    Update db.php

--- db.php
+++ db.php
@@ -220,7 +220,7 @@
      
       pg_close($db) ;
    }
-function save_esp($esp,$id){ 
+function save_esp($esp){ 
       $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
       $port        = "port=5432";
       $dbname      = "dbname=ddagopqfb1uood";
@@ -239,7 +239,7 @@ function save_esp($esp,$id){
          while($row = pg_fetch_row($ret) ){
          echo "NO max = " . $row[0] . "\n";
          $row[0] = intval($row[0]+1);
-          $sql =" INSERT INTO device (espno,espname,deviceid,password) VALUES ( ".$row[0].",'".$esp."','".$id."',"Smarthelper");";
+          $sql =" INSERT INTO device (espno,espname) VALUES ( ".$row[0].",'".$esp."');";
          }
          //echo "Records created successfully\n";
       }
@@ -253,39 +253,6 @@ function save_esp($esp,$id){
                 
          
       }
-      pg_close($db) ;
-   }
-   function update_esp($esp,$msg){
-      $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
-      $port        = "port=5432";
-      $dbname      = "dbname=ddagopqfb1uood";
-      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
-      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
-      if(!$db) {
-         echo "Error : Unable to open database\n";
-      } else {
-         echo "Opened database successfully\n";
-      }
-      $sql ="update device set password ='Smarthelper' WHERE espname='".$esp."';";
-    $ret = pg_query($db, $sql) ;
-      if(!$ret) {
-         echo pg_last_error($db) ;
-      } else {
-         $checking = 0;
-         while($row = pg_fetch_row($ret) ){
-          echo "have espname = " . $row[1] . "\n";
-           //send_LINE($msg);
-          //  check_send($row[1],$msg);
-            $checking = 1 ;
-            
-             
-         }
-         if($checking == 0){
-           //  save_esp($esp);
-         }
-         //echo "Records created successfully\n";
-      }
-     
       pg_close($db) ;
    }
    function check_esp($esp,$msg){
@@ -314,7 +281,7 @@ function save_esp($esp,$id){
              
          }
          if($checking == 0){
-             save_esp($esp,$msg);
+             save_esp($esp);
          }
          //echo "Records created successfully\n";
       }
