98b1fbd85f194e622fc4678dd7f7ba5702238fb3
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-03-25 01:32:22 +0700

    Update line.php

--- line.php
+++ line.php
@@ -170,7 +170,7 @@ function send_Menu($userid){
    $access_token = '3NZ4tPcC9W1t6cPI0r3ezvnsoK8KW04hbCSPxKSahSeGbeUU7lC8PQvx02uN5UyL7wOaVJ6EZ9oM5uQjkqLDNZtagQuRcS/NaaGmtopk7pBGOXtNk3lDc4KQIns5tV/jpm8yyr/114JL4uORE5czWwdB04t89/1O/w1cDnyilFU='; 
   $messages = [
           "type"=> "imagemap",
-         // "baseUrl"=> "https://i.imgur.com/QBHEdRu.jpg",
+          "baseUrl"=> "https://i.imgur.com/QBHEdRu.jpg",
           "altText"=> "this is a buttons template",
           "baseSize"=> [
               "height"=> 1040,
