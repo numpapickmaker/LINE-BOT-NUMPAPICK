98a68d001eb27bbc5849477388bcdd7afb1b8328
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-03-25 23:16:22 +0700

    Update login.php

--- login.php
+++ login.php
@@ -108,39 +108,126 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <html lang="en">
 <head>
      <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Kanit:200|Open+Sans|Taviraj" rel="stylesheet">
+<style>
+
+body {
+    background-color: white;
+}
+
+h1 {
+    color: #072140;
+    text-align: center;
+}
+
+p {
+    font-family: verdana;
+    font-size: 20px;
+}
+input[type=text],input[type=password], select {
+    width: 100%;
+    padding: 12px 28px;
+    margin: 8px 2px;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+
+
+.kanit {
+	font-family: 'Kanit', sans-serif;
+}.Taviraj {
+	font-family: 'Taviraj', serif;
+    font-size: 20px;
+    margin: 24px 2px;
+}
+
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: white;
+   color: black;
+   text-align: center;
+   font-family: 'Taviraj', serif;
+   font-size: 20px;
+    margin: 24px 2px;
+}
+.button {
+    background-color: white; /* Green */
+    border: none;
+	width:100%;
+    padding: 10px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-family: 'Kanit', sans-serif;
+    font-size: 24px;
+    margin: 0px 0px;
+    cursor: pointer;
+    
+}
+.button1 {
+    background-color: white; 
+    color: white; 
+    border: 2px solid white;
+    border-radius: 8px;
+  	position: relative;
+    background-color: #0F4484;
+   
+    
+}
+.button2 {
+    background-color: white; 
+    color: #072140; 
+    border: 2px solid white;
+    border-radius: 8px;
+    background-color: #FFA611;
+	position: relative;
+    
+}
+</style>
 </head>
 <body>
-    <div class="wrapper">
-        <h2>Login</h2>
-        <p>Please fill in your credentials to login.</p>
+    <div class="w3-container" >
+        <h1 class="kanit">Login</h1>
+        <ul class="w3-ul">
+    <li> </li>
+    <br>    
+        <h3 class="kanit">Please fill in your credentials to login.</h3>
+
+
+    
+   
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                <label>Device id</label>
-                <input type="text" name="username"class="form-control" value="<?php echo $username; ?>">
-                <input type="hidden" name="userid" class="form-control" value="<?php echo $userid; ?>"></input>
+                <label for="fname" class = "Taviraj" >Device id</label>
+                <input type="text" name="username"class = "Taviraj" placeholder="Device id" value="<?php echo $username; ?>">
+                <input type="hidden" name="userid" class = "Taviraj" value="<?php echo $userid; ?>"></input>
                 <span class="help-block"><?php echo $username_err; ?></span>
             </div>    
             <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control">
+                <label for="fname" class = "Taviraj" >Password</label>
+                <input type="password" name="password" class = "Taviraj" placeholder="password" >
                 <span class="help-block"><?php echo $password_err; ?></span>
             </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login">
+                       
+             <ul class="w3-ul">
+    <li> </li>
+    <br>
+                <input type="submit" class="button button1" value="Login">
                
     
-            </div>
-           
+
         </form>
         <br>
          <form action="https://numpapick.herokuapp.com/manage.php" method="get">
-    <button class="btn btn-danger" value="<?php echo $userid;?>" name="action">Back</button></form>
+    <button class="button button1" value="<?php echo $userid;?>" name="action">Back</button></form>
     </div>    
 </body>
 </html>
