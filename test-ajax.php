56ad8f4c8236995777dfb34b3c36f38a942dda8b
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-03-25 18:20:18 +0700

    Update test-ajax.php

--- test-ajax.php
+++ test-ajax.php
@@ -1,68 +1,61 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>ThaiCreate.Com Ajax Tutorial</title>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-874" />
+<title>ajax ของง่ายถ้าใช้ jQuery</title>
+ 
+<script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
+<script type="text/javascript">
+// ทดสอบการใช้ GET
+function getData(){
+	$.get("get.php", { data: $("#data1").val()}, 
+		function(data){
+			$("#divGetData").html(data);
+		}
+	);
+}
+ 
+// ทดสอบการใช้ POST
+function postData(){
+	$.post("post.php", { data: $("#data2").val()}, 
+		function(data){
+			$("#divPostData").html(data);
+		}
+	);
+}
+ 
+// ทดสอบการใช้ LOAD
+function loadData(){
+	$("#divLoadData").load("load.php?data="+$("#data3").val());
+}
+ 
+ 
+</script>
+ 
 </head>
-	<script language="JavaScript">
-	   var HttPRequest = false;
-
-	   function doCallAjax() {
-		  HttPRequest = false;
-		  if (window.XMLHttpRequest) { // Mozilla, Safari,...
-			 HttPRequest = new XMLHttpRequest();
-			 if (HttPRequest.overrideMimeType) {
-				HttPRequest.overrideMimeType('text/html');
-			 }
-		  } else if (window.ActiveXObject) { // IE
-			 try {
-				HttPRequest = new ActiveXObject("Msxml2.XMLHTTP");
-			 } catch (e) {
-				try {
-				   HttPRequest = new ActiveXObject("Microsoft.XMLHTTP");
-				} catch (e) {}
-			 }
-		  } 
-		  
-		  if (!HttPRequest) {
-			 alert('Cannot create XMLHTTP instance');
-			 return false;
-		  }
-	
-			var url = 'test-ajax2.php';
-			var pmeters = 'myName='+document.getElementById("txtName").value;
-			//var pmeters = 'myName='+document.getElementById("txtName").value+'&my2='; // 2 Parameters
-			HttPRequest.open('POST',url,true);
-
-			HttPRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			HttPRequest.setRequestHeader("Content-length", pmeters.length);
-			HttPRequest.setRequestHeader("Connection", "close");
-			HttPRequest.send(pmeters);
-			
-			
-			HttPRequest.onreadystatechange = function()
-			{
-
-				 if(HttPRequest.readyState == 3)  // Loading Request
-				  {
-				   document.getElementById("mySpan").innerHTML = "Now is Loading...";
-				  }
-
-				 if(HttPRequest.readyState == 4) // Return Request
-				  {
-				   window.location = "test-ajax2.php";
-				  }
-				
-			}
-
-			/*
-			HttPRequest.onreadystatechange = call function .... // Call other function
-			*/
-
-	   }
-	</script>	
-<body>	
-	<input type="text" name="txtName" id="txtName" value="">
-	<input name="btnButton" id="btnButton" type="button" value="Click" onClick="JavaScript:doCallAjax();">
-	<br>
-	<span id="mySpan"></span>
+ 
+<body>
+<form id="form1" name="form1" method="post" action="">
+  <div id="divGetData"></div>
+  <input name="data1" type="text" id="data1" size="40" />
+  <input type="button" name="Button1" id="button1" value="Get" onclick="getData()" />
+</form>
+ 
+<hr />
+ 
+<form id="form2" name="form2" method="post" action="">
+  <div id="divPostData"></div>
+  <input name="data2" type="text" id="data2" size="40" />
+  <input type="button" name="Button2" id="button2" value="Post" onclick="postData()" />
+</form>
+ 
+<hr />
+ 
+<form id="form3" name="form3" method="post" action="">
+  <div id="divLoadData"></div>
+  <input name="data3" type="text" id="data3" size="40" />
+  <input type="button" name="Button3" id="button3" value="Load" onclick="loadData()" />
+</form>
+ 
 </body>
 </html>
