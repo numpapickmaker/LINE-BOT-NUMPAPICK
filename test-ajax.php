897087d76424127c200e6bcd979f69e1a32279c3
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-03-25 18:00:08 +0700

    Update test-ajax.php

--- test-ajax.php
+++ test-ajax.php
@@ -1,12 +1,8 @@
-<?php
-	/*** By Weerachai Nukitram***/
-	/***  http://www.ThaiCreate.Com ***/	
-?>
 <html>
 <head>
 <title>ThaiCreate.Com Ajax Tutorial</title>
 </head>
-<script language="JavaScript">
+	<script language="JavaScript">
 	   var HttPRequest = false;
 
 	   function doCallAjax() {
@@ -31,10 +27,9 @@
 			 return false;
 		  }
 	
-		  var url = 'test-ajax2.php';
-		  var pmeters = "tUsername=" + encodeURI( document.getElementById("txtUsername").value) +
-						"&tPassword=" + encodeURI( document.getElementById("txtPassword").value );
-
+			var url = 'test-ajax2.php';
+			var pmeters = 'myName='+document.getElementById("txtName").value;
+			//var pmeters = 'myName='+document.getElementById("txtName").value+'&my2='; // 2 Parameters
 			HttPRequest.open('POST',url,true);
 
 			HttPRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -46,45 +41,28 @@
 			HttPRequest.onreadystatechange = function()
 			{
 
-				if(HttPRequest.readyState == 3)  // Loading Request
-				{
-					document.getElementById("mySpan").innerHTML = "Now is Loading...";
-				}
+				 if(HttPRequest.readyState == 3)  // Loading Request
+				  {
+				   document.getElementById("mySpan").innerHTML = "Now is Loading...";
+				  }
 
-				if(HttPRequest.readyState == 4) // Return Request
-				{
-					if(HttPRequest.responseText == 'Y')
-					{
-						window.location = 'AjaxPHPLoginForm3.php';
-					}
-					else
-					{
-						document.getElementById("mySpan").innerHTML = HttPRequest.responseText;
-					}
-				}
+				 if(HttPRequest.readyState == 4) // Return Request
+				  {
+				   document.getElementById("mySpan").innerHTML = HttPRequest.responseText;
+				  }
 				
 			}
 
+			/*
+			HttPRequest.onreadystatechange = call function .... // Call other function
+			*/
+
 	   }
-	</script>
-<body>
-<h1>Login Form</h1>
-<form name="frmMain">
-<span id="mySpan"></span>
-<table width="274" border="1">
-  <tr>
-    <th width="117">
-      <div align="left">Username</div></th>
-    <th><input type="text" name="txtUsername" id="txtUsername" size="20"></th>
-  </tr>
-  <tr>
-    <th width="117">
-      <div align="left">Password</div></th>
-    <th><input type="password" name="txtPassword" id="txtPassword" size="20"></th>
-  </tr> 
-</table>
-<br>
-<input name="btnLogin" type="button" id="btnLogin" OnClick="JavaScript:doCallAjax();" value="Login">
-</form>
+	</script>	
+<body>	
+	<input type="text" name="txtName" id="txtName" value="">
+	<input name="btnButton" id="btnButton" type="button" value="Click" onClick="JavaScript:doCallAjax();">
+	<br>
+	<span id="mySpan"></span>
 </body>
 </html>
