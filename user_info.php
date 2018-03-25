cb1c75a808cb37855c4bc7d9c6b3d7db40b5631e
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-03-26 02:58:58 +0700

    Update user_info.php

--- user_info.php
+++ user_info.php
@@ -127,7 +127,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <html>
 <head>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-  <link href="https://fonts.googleapis.com/css?family=Kanit:200|Open+Sans|Taviraj" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Kanit:200|Open+Sans|Taviraj" rel="stylesheet">
 <style>
 
 body {
