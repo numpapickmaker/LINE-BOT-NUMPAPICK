67372cefdc2aae75df4ca1af143b4d8924a83cd9
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-03-26 16:35:35 +0700

    Update line.php

--- line.php
+++ line.php
@@ -113,7 +113,7 @@ function send_PRESS($userid){
                    [
                     "type"=> "uri",
                     "label"=> "View detail",
-                    "uri"=> "http://app.midatdb.com/PFrCbTJd2J"
+                    "uri"=> "tel:0846823134"
                   ]
               ]
           ]
