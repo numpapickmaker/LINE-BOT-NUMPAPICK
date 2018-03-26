d0cb92c72d35aacacefdf92f90f5be316661550c
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-03-26 21:56:26 +0700

    Update main.php

--- main.php
+++ main.php
@@ -86,7 +86,7 @@ input[type=text], select {
           
           $checking = 1;
          }
-         if($checking != 0 ){
+         if($checking == 0 ){
           header("Location: https://numpapick.herokuapp.com/user_info.php");
          //echo "Records created successfully\n";
        }
