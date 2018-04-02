d1ce674f624961d6e5c30abfc7adab16f1426cf1
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-04-03 02:12:31 +0700

    Update information.php

--- information.php
+++ information.php
@@ -25,7 +25,7 @@ if( $_GET["name"]|| $_GET["birthday"]|| $_GET["sex"]|| $_GET["heigth"]|| $_GET["
 }
 //echo $_GET["view"];
   $Test = $_GET["view"];
-  $No =$Device_id =$Name =$Birthday =$Sex =$Heigth =$Weigth =$disease =$address = $phone = "";
+  $No =$Device_id =$Name =$Birthday =$Sex =$Heigth =$Weigth =$disease =$address = $phone =   $Password = "";
  $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
       $port        = "port=5432";
       $dbname      = "dbname=ddagopqfb1uood";
@@ -61,6 +61,23 @@ if( $_GET["name"]|| $_GET["birthday"]|| $_GET["sex"]|| $_GET["heigth"]|| $_GET["
             // $username_err = 'No account found with that username.';
          }
          //echo "Records created successfully\n";
+          $sql ="SELECT * FROM device WHERE Deviceid='".$_GET["view"]."';";
+    $ret = pg_query($db, $sql) ;
+      if(!$ret) {
+         echo pg_last_error($db) ;
+      } else {
+         $checking = 0;
+         while($row = pg_fetch_row($ret) ){
+        //  echo "have espname = " . $row[1] . "\n";
+          // send_LINE('PASS');
+          //  header("location: bot.php");
+          
+           $Password = $row[3];
+          
+         }
+         if($checking == 0){
+            // $username_err = 'No account found with that username.';
+         }
       }
      
       pg_close($db) ;   
@@ -204,6 +221,8 @@ function Edit() {
             var f = document.getElementById("disease").innerHTML;
             var g = document.getElementById("address").innerHTML;
             var h = document.getElementById("phone").innerHTML;
+             var pw = document.getElementById("phone").innerHTML;
+            document.getElementById("Password").innerHTML=''+pw+'<form action="https://numpapick.herokuapp.com/password.php" method="get">  <input type="hidden" name="username" class = "Taviraj" value="<?php echo $_GET["view"];?>" > <button  value="<?php echo $_GET["username"];?>"name="action">change password</button></form> ';
             document.getElementById("name").innerHTML='<input id="namefld" type="text" value="'+a+'"'+' />';
             document.getElementById("birthday").innerHTML='<input id="birthdayfld" type="date" value="'+b+'"'+' />';
             document.getElementById("sex").innerHTML='<input id="sexfld" type="radio" value="male" name="sexfld"'+male+' />Male <input id="sexfld" type="radio" value="female" name="sexfld" '+female+'/> Female ';
@@ -244,6 +263,10 @@ function Edit() {
           <td  ><div align="center">Device id </div></td>
           <td id="Device_id" ><?php echo $Device_id;?></td>
               
+          </tr>
+          <tr>
+          <td><div align="center">Password </div></td>
+          <td id="Password"><?php echo $Password;?></td>
           </tr>
           <tr>
           <td><div align="center">Name </div></td>
