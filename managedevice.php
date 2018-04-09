4b12622bf816d5ffb1f33d91a3e611fccb61d81d
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-04-10 02:50:29 +0700

    Update managedevice.php

--- managedevice.php
+++ managedevice.php
@@ -1,215 +1,324 @@
-<?php
-// Include config file
-//cho $_GET["add"];
-// Define variables and initialize with empty values
-//$username = $password = "";
-//$username_err = $password_err = "";
-//$userid = $_GET["add"];
-// Processing form data when form is submitted
-$username = $_REQUEST["uname"];
-$password = $_REQUEST["psw"];
-$userid = $_REQUEST["userid"];
-$result = "";            
-if( !empty($username) && !empty($password)){   
-      $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
-      $port        = "port=5432";
-      $dbname      = "dbname=ddagopqfb1uood";
-      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
-      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
-      if(!$db) {
-       //  echo "Error : Unable to open database\n";
-      } else {
-         //echo "Opened database successfully\n";
-      }
-      $sql ="SELECT * FROM device WHERE deviceid='".$username."' AND password='".$password."';";
-    $ret = pg_query($db, $sql) ;
-      if(!$ret) {
-         echo pg_last_error($db) ;
-      } else {
-         $checking = 0;
-         while($row = pg_fetch_row($ret) ){
-          //echo "have espname = " . $row[1] . "\n";
-          // send_LINE('PASS');
-            $checking = 1;
-            
-             
-         }
-         if($checking == 0){
-             $result = 'username or password is wrong.';
-         }else{
-              $sql ="SELECT * FROM userline WHERE id='".$userid."' AND esp='".$username."';";
-              $ret = pg_query($db, $sql) ;
-              if(!$ret) {
-                //  echo pg_last_error($db) ;
-              } else {
-                  $checking = 0;   
-                   while($row = pg_fetch_row($ret)){
-                     //  echo "ESP name = " . $row[2] . "\n";
-                       // send_LINE('PASS')
-                        $checking = 1;     
-                        //send_LINE("you login already",$userid);
-                       // echo "string";     
-                     }  
-                    
-                if( $checking == 0){
-                           $sql ="SELECT MAX(userno) as LargestNO from userline;";
-                      $ret = pg_query($db, $sql) ;
-                      if(!$ret) {
-                        echo pg_last_error($db) ;
-                      } else {
-                          while($row = pg_fetch_row($ret) ){
-                         // echo "NO max = " . $row[0] . "\n";
-                          $row[0] = intval($row[0]+1);
-                          $sql =" INSERT INTO userline (userno,id,esp) VALUES ( ".$row[0].",'".$userid."','".$username."');";
-                      }
-                      $ret = pg_query($db, $sql) ;
-                      if(!$ret) {
-                      //  send_LINE("Login Error!",$userid);
-                            echo pg_last_error($db) ;
-                      } else {
-                    //send_LINE("Login success",$userid);
-                          //  echo "Records created successfully\n";
-                    //getMqttfromlineMsg("555");
-                         // header("location: manage.php?action=$userid");
-                        $result = "success";
-                      }
-                  //echo "Records created successfully\n";
-                  }
-                }else{
-                  $result = "you have been login";
-                }
-              }
-         
-                  
-           }
-         //echo "Records created successfully\n";
-      }
-      echo $result ;
-      pg_close($db) ;
-}   
-
-// Include config file
-//cho $_GET["add"];
-// Define variables and initialize with empty values
-$firstname = $_REQUEST["fname"];
-$lastname = $_REQUEST["lname"];
-$phone=$_REQUEST["phone"];
-$email = $_REQUEST["email"];
-$career= $_REQUEST["career"];
-$birthday =$_REQUEST["birthday"];
-
-$firstname_err = $lastname_err =  $phone_err=$email_err = $career_err= $birthday_err= "";
-
-
-// Processing form data when form is submitted
-if(!empty($firstname)){
-   
-      $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
-      $port        = "port=5432";
-      $dbname      = "dbname=ddagopqfb1uood";
-      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
-      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
-      if(!$db) {
-         echo "Error : Unable to open database\n";
-      } else {
-         //echo "Opened database successfully\n";
-      }
-       $sql ="SELECT * FROM user_info WHERE id='".$userid."';";
-        $ret = pg_query($db, $sql) ;
-      if(!$ret) {
-         echo pg_last_error($db) ;
-      } else {
-          $check_id = 0;
-         while($row = pg_fetch_row($ret) ){
-            $check_id=1;
-            // echo "check_id ==1";
-         }
-         if($check_id == 0){
-          // echo "check_id ==0";
-                $sql ="SELECT MAX(user_no) as LargestNO from user_info;";
-                $ret = pg_query($db, $sql) ;
-                if(!$ret) {
-                   echo pg_last_error($db) ;
-                } else {
-                 
-                   while($row = pg_fetch_row($ret) ){
-                      $row[0] = intval($row[0]+1);
-                      $sql ="insert into user_info values (".$row[0].",'".$userid."','".$firstname."','".$lastname."','".$phone."','".$email."','".$career."','".$birthday."');";
-                      $result = $sql;
-                       
-                   }
-                  
-                        
-                        $ret = pg_query($db, $sql) ;
-                        if(!$ret) {
-                            echo pg_last_error($db) ;
-                        } else {
-                            $checking = 0;   
-                             while($row = pg_fetch_row($ret)){
-                               //  echo "ESP name = " . $row[2] . "\n";
-                                 // send_LINE('PASS')
-                                  $checking = 1;     
-                                  //send_LINE("you login already",$userid);
-                                 // echo "string";     
-                               }  
-                              // header("location: https://numpapick.herokuapp.com/main.php?action=$userid");
-                             $result = "success";
-                          }
-               
-               
-              }
-         }
-      }
-    echo $result;
-    pg_close($db) ;
-      
-  }    
+<?php 
+$userid = $_GET["userid"];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/solid.js" integrity="sha384-+Ga2s7YBbhOD6nie0DzrZpJes+b2K1xkpKxTFFcx59QmVPaSA8c7pycsNaFwUK6l" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/fontawesome.js" integrity="sha384-7ox8Q2yzO/uWircfojVuCQOZl+ZZBg2D2J5nkpLqzH1HY0C1dHlTKIbpRz/LG23c" crossorigin="anonymous"></script>
+	<link href="https://fonts.googleapis.com/css?family=Kanit:200|Open+Sans|Taviraj" rel="stylesheet">
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
  
-$create_table = $_REQUEST["create"]; 
-if(!empty($create_table)){
-
-//echo $_GET["action"];
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
   
-  $No =$Device_id =$Name =$Age =$Sex =$Heigth =$Weigth =$disease =$address = $phone = "";
- $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
-      $port        = "port=5432";
-      $dbname      = "dbname=ddagopqfb1uood";
-      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
-      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
-      if(!$db) {
-         echo "Error : Unable to open database\n";
-      } else {
-         //echo "Opened database successfully\n";
-      }
-     $sql ="SELECT device_information.device_id , device_information.name from device_information inner join userline on device_information.device_id =userline.esp where userline.id='".$userid."';";
-     $ret = pg_query($db, $sql) ;
-      if(!$ret) {
-         echo pg_last_error($db) ;
-      } else {
-         $checking = 0;
-         $n = 1;
-         
-         while($row = pg_fetch_row($ret) ){
-           //echo "have espname = " . $row[1] . "\n";
-          // send_LINE('PASS');
-          //  header("location: bot.php");
+<style>
+	
+body {
+    background-color: white;
+}
 
-            $result =+ "<tr>";
-            $result =+ "<td>".$row[0]."</td>";
-            $result =+ "<td>".$row[1]."</td>";
-            $result =+ '<td><button class="prevbutton" style = "font-size: 18px; width:80% ;margin: 0px 0px;"> แก้ไข</button></td> </tr>';
+h1 {
+    color: #072140;
+    text-align: center;
+}
+.table {
+    border-radius: 5px;
+    width: 100%;
+    margin: 0px auto;
+    float: none;
+}
+.table td {
+   text-align: center;   
+}
+p {
+    font-family: verdana;
+    font-size: 20px;
+}
+/* Full-width input fields */
 
-         }
-         if($checking == 0){
-            // $username_err = 'No account found with that username.';
+.kanit {
+	font-family: 'Kanit', sans-serif;
+}.Taviraj {
+	font-family: 'Taviraj', serif;
+    font-size: 18px;
+    margin: 24px 2px;
+}
+.prevbutton {
+    background-color: white; 
+    border: none;
+    color: #072140;
+    padding: 10px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-family: 'Kanit', sans-serif;
+    font-size: 24px;
+    margin: 2px 2px;
+    cursor: pointer;
+    width: 50%;
+    border-radius: 8px;
+    border: 2px solid #072140;
+}
+.nextbutton{
+	 background-color: #072140; 
+    border: none;
+    color: white;
+    padding: 10px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-family: 'Kanit', sans-serif;
+    font-size: 24px;
+    margin: 2px 2px;
+    cursor: pointer;
+    width: 50%;
+    border-radius: 8px;
+    border: 2px solid #072140;
+}
+.addbutton{
+	 background-color: white; 
+    border: none;
+    color: #FFA400;
+    padding: 10px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-family: 'Kanit', sans-serif;
+    font-size: 24px;
+    margin: 2px 2px;
+    cursor: pointer;
+    width: 50%;
+    border-radius: 8px;
+    border: 2px solid #FFA400;
+}
+.Row {
+            
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: white;
+   color: black;
+   text-align: center;
+   font-size: 20px;
+    margin: 24px 2px;
+        }
+.Row.Expand {
+             height: auto;
+        }
+.step {
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbbbbb;
+  border: none;  
+  border-radius: 50%;
+  display: inline-block;
+  opacity: 0.5;
+}
 
-         }
-         //echo "Records created successfully\n";
-      }
-     echo $result;
-      pg_close($db) ;   
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+/* Set a style for all buttons */
+
+
+.container {
+    padding: 16px;
+}
+
+span.psw {
+    float: right;
+    padding-top: 16px;
+}
+
+/* The Modal (background) */
+.modal {
+    display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+    padding-top: 60px;
+}
+
+/* Modal Content/Box */
+.modal-content {
+    background-color: #fefefe;
+    margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+    border: 1px solid #888;
+    width: 80%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button (x) */
+.close {
+    position: absolute;
+    right: 25px;
+    top: 0;
+    color: #000;
+    font-size: 35px;
+    font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+    color: red;
+    cursor: pointer;
+}
+
+/* Add Zoom Animation */
+.animate {
+    -webkit-animation: animatezoom 0.6s;
+    animation: animatezoom 0.6s
+}
+
+</style>
+</head>
+<body>
+
+<p>
+<h1 class="kanit">จัดการข้อมูลผู้ใช้งาน</h1>
+<p>   
+  
+<div style="text-align: center; ">
+  <span class="step"></span>
+    <span class="step" ></span>
+    <span class="step" style="background-color: #000000;"></span>
+    
+    <div class="container" >
+                 
+  <table width="100%" class="table table-striped  Taviraj">
+    <thead>
+      <tr>
+        <th>หมายเลข</th>
+        <th>ผู้ใช้งาน</th>
+        <th>แก้ไข</th>
+      </tr>
+    </thead>
+    <tbody>
+    
+    <div id="listdevice">
+    
+     </div>
+       </tbody>
+       
+  </table>
+   
+</div>
+	
+    <p><button class="addbutton" 	 onclick="document.getElementById('id01').style.display='block'">เพิ่ม</button></p>
+    <p><button class="prevbutton" >ย้อนกลับ</button></p>
+	<p><button class="nextbutton" onclick="document.getElementById('id02').style.display='block'">ถัดไป</button></p>
+	
+ <input type="hidden" name="userid" id="userid" value="<?php echo $userid;?>">
+ <!--<div class="Row">
+ <div style="text-align:center;margin-top:40px;">
+    <span class="step" style="background-color: #000000;"></span>
+    <span class="step"></span>
+    <span class="step"></span>
+    <span class="step"></span>
+  </div>
+</div> -->
+
+<div id="id01" class="modal">
+  
+  <form class="modal-content animate" action="/action_page.php">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+    </div>
+
+    <div class="container Taviraj" >
+      <p>
+      <label for="uname"><b>อุปกรณ์</b></label>
+      <input type="text" placeholder="เลขอุปกรณ์" name="uname" required>
+	  <p>
+      <label for="psw"><b>รหัสผ่าน</b></label>
+      <input type="password" placeholder="รหัสผ่านเดิม" name="psw" required>
+      <input type="password" placeholder="รหัสผ่านใหม่" name="psw2" required>
+      <input  type="password" placeholder="ยืนยันรหัสผ่านใหม่" name="psw3" required>
+      <p>
+      <label for="psw"><b>ข้อมูลผู้สูงอายุ</b></label>
+      <input  type="password" placeholder="ชื่อ" name="psw4" required>
+      <input  type="password" placeholder="นามสกุล" name="psw5" required>
+      <input  type="password" placeholder="........" name="psw6" required>
+      <p><p><p><p>
+      <button class="prevbutton" style = "font-size: 18px; width:40%"> ย้อนกลับ</button>
+     <button class="nextbutton" style = "font-size: 18px; width:40%"> ถัดไป</button>
+    
+    </div>
+
+   
+  </form>
+</div>
+
+
+
+<div id="id02" class="modal">
+  
+  <form class="modal-content animate" action="/action_page.php">
+    
+
+    <div class="container Taviraj" >
+      <p>
       
+     <h1 class= "kanit">เริ่มต้นใช้งานสำเสร็จ</h1>
+     <p>
+     <i class="fas fa-check" style ="width: 100px; height: 100px; color: 	#32CD32;"></i>
+     <p>
+     <button class="nextbutton container Taviraj"  style = "font-size: 24px; width:50% ; background-color: 	#32CD32; border: 2px solid white;">  เสร็จสิ้น </button>
+     </div>
+   </form>
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+var modal = document.getElementById('id02');
+var modal = document.getElementById('id03');
+create_table();
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+function edit(){
+	var userid = document.getElementById('userid').value;
+    var deviceid = document.getElementById('deviceid').value.value;
+}
+function submit(){
+	var userid = document.getElementById('userid').value;
+}
+
+function create_table(){
+	var userid = document.getElementById('userid').value;
+     document.getElementById("listdevice").innerHTML = userid;
+    var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("listdevice").innerHTML = this.responseText;
+                    
+                }
+            };
+            xmlhttp.open("GET", "https://numpapick.herokuapp.com/add_device.php?userid=" + userid + "&create=1", true);
+            xmlhttp.send();
 
 }
-?>
- 
+
+</script>
+</body>
+</html>
+
