0cd67decea95c5d9eb17e15f22f98ff1f9540d6a
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-04-09 23:04:41 +0700

    Update user_info.php

--- user_info.php
+++ user_info.php
@@ -248,7 +248,7 @@ function user(){
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("uname_error").innerHTML = this.responseText;
+                document.getElementById("birthday_error").innerHTML = this.responseText;
                  
             }
         };
