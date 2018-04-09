dd624543942800c044290728c5c734c334a81062
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-04-09 14:20:59 +0700

    Update user_info.php

--- user_info.php
+++ user_info.php
@@ -109,40 +109,55 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Kanit:200|Open+Sans|Taviraj" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Kanit:200|Open+Sans|Taviraj" rel="stylesheet">
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+ 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
 <style>
 body {
     background-color: white;
 }
+
 h1 {
     color: #072140;
     text-align: center;
 }
+
 p {
     font-family: verdana;
     font-size: 20px;
 }
-input[type=text],input[type=date], select {
-    width: 100%;
-    padding: 12px 28px;
-    margin: 8px 2px;
-    display: inline-block;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    box-sizing: border-box;
-}
+/* Full-width input fields */
+
 .kanit {
-  font-family: 'Kanit', sans-serif;
+	font-family: 'Kanit', sans-serif;
 }.Taviraj {
-  font-family: 'Taviraj', serif;
-    font-size: 20px;
+	font-family: 'Taviraj', serif;
+    font-size: 18px;
     margin: 24px 2px;
 }
-.button {
-    background-color: #072140; 
+.prevbutton {
+    background-color: white; 
+    border: none;
+    color: #072140;
+    padding: 10px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-family: 'Kanit', sans-serif;
+    font-size: 24px;
+    margin: 2px 2px;
+    cursor: pointer;
+    width: 50%;
+    border-radius: 8px;
+    border: 2px solid #072140;
+}
+.nextbutton{
+	 background-color: #072140; 
     border: none;
     color: white;
     padding: 10px 32px;
@@ -151,34 +166,132 @@ input[type=text],input[type=date], select {
     display: inline-block;
     font-family: 'Kanit', sans-serif;
     font-size: 24px;
-    margin: 24px 2px;
+    margin: 2px 2px;
     cursor: pointer;
-    
-}
-.button1 {
-    background-color: white; 
-    color: #072140; 
-    border: 2px solid #072140;
+    width: 50%;
     border-radius: 8px;
-    width: 100%;
-    
+    border: 2px solid #072140;
 }
-.footer {
-   position: fixed;
+.addbutton{
+	 background-color: white; 
+    border: none;
+    color: #FFA400;
+    padding: 10px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-family: 'Kanit', sans-serif;
+    font-size: 24px;
+    margin: 2px 2px;
+    cursor: pointer;
+    width: 50%;
+    border-radius: 8px;
+    border: 2px solid #FFA400;
+}
+.Row {
+            
    left: 0;
    bottom: 0;
    width: 100%;
    background-color: white;
    color: black;
    text-align: center;
-   font-family: 'Taviraj', serif;
    font-size: 20px;
     margin: 24px 2px;
+        }
+.Row.Expand {
+             height: auto;
+        }
+.step {
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbbbbb;
+  border: none;  
+  border-radius: 50%;
+  display: inline-block;
+  opacity: 0.5;
 }
+
+input[type=text], select {
+    width: 100%;
+    padding: 12px 28px;
+    margin: 8px 2px;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+
+/* Set a style for all buttons */
+
+
+.container {
+    padding: 16px;
+}
+
+span.psw {
+    float: right;
+    padding-top: 16px;
+}
+
+/* The Modal (background) */
+.modal {
+    display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+    padding-top: 60px;
+}
+
+/* Modal Content/Box */
+.modal-content {
+    background-color: #fefefe;
+    margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+    border: 1px solid #888;
+    width: 80%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button (x) */
+.close {
+    position: absolute;
+    right: 25px;
+    top: 0;
+    color: #000;
+    font-size: 35px;
+    font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+    color: red;
+    cursor: pointer;
+}
+
+/* Add Zoom Animation */
+.animate {
+    -webkit-animation: animatezoom 0.6s;
+    animation: animatezoom 0.6s
+}
+
 </style>
 </head>
 <body>
 <h1 class="kanit">กรอกข้อมูลเพื่อยืนยันตัวตน</h1>
+
+<p>
+    <div style="text-align: center;">
+    <span class="step" style="background-color: #000000;"></span>
+    <span class="step" ></span>
+    <span class="step"></span>
+    <p>
+    </div>
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
 
     <label for="fname" class = "Taviraj" >กรอกประวัติผู้ดูแล</label>
@@ -206,9 +319,12 @@ input[type=text],input[type=date], select {
   <p class="Taviraj">วัดเกิด</p> <input type="Date" id="fname" name="birthday"  class = "Taviraj" value="<?php echo $birthday; ?>"> 
   <span class="help-block"><?php echo $birthday_err; ?></span>
   </div>
+  <div style="text-align: center;">
    <input type="hidden" name="action" value="<?php echo $userid; ?>">
-  <input type="submit" class="button button1" value="Next"> 
+  <input type="submit" class="nextbutton" value="Next"> 
+  </div>
   </form>
+
 
 <div class="footer">
   <p>   </p>
