e0ecdcf9ebc5542084791554607163fa37e48311
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-04-09 16:44:20 +0700

    Update and rename login_effect.js to add_device.php

---
+++ add_device.php
@@ -0,0 +1,90 @@
+<?php
+// Include config file
+//cho $_GET["add"];
+// Define variables and initialize with empty values
+//$username = $password = "";
+//$username_err = $password_err = "";
+//$userid = $_GET["add"];
+// Processing form data when form is submitted
+$username = $_REQUEST["uname"];
+$password = $_REQUEST["psw"];
+$result = "";            
+   
+      $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
+      $port        = "port=5432";
+      $dbname      = "dbname=ddagopqfb1uood";
+      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
+      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
+      if(!$db) {
+       //  echo "Error : Unable to open database\n";
+      } else {
+         //echo "Opened database successfully\n";
+      }
+      $sql ="SELECT * FROM device WHERE deviceid='".$username."' AND password='".$password."';";
+    $ret = pg_query($db, $sql) ;
+      if(!$ret) {
+         echo pg_last_error($db) ;
+      } else {
+         $checking = 0;
+         while($row = pg_fetch_row($ret) ){
+          echo "have espname = " . $row[1] . "\n";
+          // send_LINE('PASS');
+            $checking = 1;
+            
+             
+         }
+         if($checking == 0){
+             $result = 'username or password is wrong.';
+         }else{
+              $sql ="SELECT * FROM userline WHERE id='".$userid."' AND esp='".$username."';";
+              $ret = pg_query($db, $sql) ;
+              if(!$ret) {
+                //  echo pg_last_error($db) ;
+              } else {
+                  $checking = 0;   
+                   while($row = pg_fetch_row($ret)){
+                     //  echo "ESP name = " . $row[2] . "\n";
+                       // send_LINE('PASS')
+                        $checking = 1;     
+                        //send_LINE("you login already",$userid);
+                       // echo "string";     
+                     }  
+                    
+                if( $checking == 0){
+                           $sql ="SELECT MAX(userno) as LargestNO from userline;";
+                      $ret = pg_query($db, $sql) ;
+                      if(!$ret) {
+                        echo pg_last_error($db) ;
+                      } else {
+                          while($row = pg_fetch_row($ret) ){
+                         // echo "NO max = " . $row[0] . "\n";
+                          $row[0] = intval($row[0]+1);
+                          $sql =" INSERT INTO userline (userno,id,esp) VALUES ( ".$row[0].",'".$userid."','".$username."');";
+                      }
+                      $ret = pg_query($db, $sql) ;
+                      if(!$ret) {
+                      //  send_LINE("Login Error!",$userid);
+                            echo pg_last_error($db) ;
+                      } else {
+                    //send_LINE("Login success",$userid);
+                          //  echo "Records created successfully\n";
+                    //getMqttfromlineMsg("555");
+                         // header("location: manage.php?action=$userid");
+                        $result = "success";
+                      }
+                  //echo "Records created successfully\n";
+                  }
+                }else{
+                  $result = "you have been login";
+                }
+              }
+         
+                  
+           }
+         //echo "Records created successfully\n";
+      }
+      echo $result ;
+      pg_close($db) ;
+   
+?>
+ 
