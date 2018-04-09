aa1999c1cbfbda6ae8fc09dfa3b2bc304c78563e
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-04-09 22:47:22 +0700

    Update add_device.php

--- add_device.php
+++ add_device.php
@@ -10,7 +10,7 @@ $username = $_REQUEST["uname"];
 $password = $_REQUEST["psw"];
 $userid = $_REQUEST["userid"];
 $result = "";            
-  
+if( !empty($username) && !empty($password)){   
       $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
       $port        = "port=5432";
       $dbname      = "dbname=ddagopqfb1uood";
@@ -86,9 +86,84 @@ $result = "";
       }
       echo $result ;
       pg_close($db) ;
-  
+}   
 
+// Include config file
+//cho $_GET["add"];
+// Define variables and initialize with empty values
+$firstname = $_REQUEST["fname"];
+$lastname = $_REQUEST["lname"];
+$phone=$_REQUEST["phone"];
+$email = $_REQUEST["email"];
+$career= $_REQUEST["career"];
+$birthday =$_REQUEST["birthday"];
+
+$firstname_err = $lastname_err =  $phone_err=$email_err = $career_err= $birthday_err= "";
+$userid = $_REQUEST["userid"];
+echo $userid;
+// Processing form data when form is submitted
+if(!empty($firstname)){
    
+      $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
+      $port        = "port=5432";
+      $dbname      = "dbname=ddagopqfb1uood";
+      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
+      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
+      if(!$db) {
+         echo "Error : Unable to open database\n";
+      } else {
+         //echo "Opened database successfully\n";
+      }
+       $sql ="SELECT * FROM user_info WHERE id='".$userid."';";
+        $ret = pg_query($db, $sql) ;
+      if(!$ret) {
+         echo pg_last_error($db) ;
+      } else {
+          $check_id = 0;
+         while($row = pg_fetch_row($ret) ){
+            $check_id=1;
+            // echo "check_id ==1";
+         }
+         if($check_id == 0){
+          // echo "check_id ==0";
+                $sql ="SELECT MAX(user_no) as LargestNO from user_info;";
+                $ret = pg_query($db, $sql) ;
+                if(!$ret) {
+                   echo pg_last_error($db) ;
+                } else {
+                 
+                   while($row = pg_fetch_row($ret) ){
+                      $row[0] = intval($row[0]+1);
+                      $sql ="insert into user_info values (".$row[0].",'".$userid."','".$firstname."','".$lastname."','".$phone."','".$email."','".$career."','".$birthday."');";
+                      
+                       
+                   }
+                  
+                        
+                        $ret = pg_query($db, $sql) ;
+                        if(!$ret) {
+                            echo pg_last_error($db) ;
+                        } else {
+                            $checking = 0;   
+                             while($row = pg_fetch_row($ret)){
+                               //  echo "ESP name = " . $row[2] . "\n";
+                                 // send_LINE('PASS')
+                                  $checking = 1;     
+                                  //send_LINE("you login already",$userid);
+                                 // echo "string";     
+                               }  
+                              // header("location: https://numpapick.herokuapp.com/main.php?action=$userid");
+                             $result = "success";
+                          }
+               
+               
+              }
+         }
+      }
+    echo $result;
+    pg_close($db) ;
+      
+  }    
  
 
 ?>
