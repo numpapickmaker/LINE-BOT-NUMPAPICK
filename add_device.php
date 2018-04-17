76701ce97cb35fe4da798dc99cde627cfbdf469a
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-04-17 20:13:56 +0700

    Update add_device.php

--- add_device.php
+++ add_device.php
@@ -9,6 +9,7 @@
 $username = $_REQUEST["uname"];
 $password = $_REQUEST["psw"];
 $userid = $_REQUEST["userid"];
+$deviceid = $_REQUEST["deviceid"];
 $result = "";            
 if( !empty($username) && !empty($password)){   
       $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
@@ -216,6 +217,105 @@ if(!empty($create_table)){
          //echo "Records created successfully\n";
       }
      //echo $result;
+      pg_close($db) ;   
+      
+}
+$elderinfo = $_REQUEST["elderinfo"]; 
+if(!empty($elderinfo)){
+//echo $_GET["action"];
+/*   $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
+      $port        = "port=5432";
+      $dbname      = "dbname=ddagopqfb1uood";
+      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
+      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
+      if(!$db) {
+         echo "Error : Unable to open database\n";
+      } else {
+         //echo "Opened database successfully\n";
+      }
+     $sql ="update Device_information set name='".$_GET["name"]."',sex='".$_GET["sex"]."',heigth='".$_GET["heigth"]."',weigth='".$_GET["weigth"]."',disease='".$_GET["disease"]."' ,address='".$_GET["address"]."',phone='".$_GET["phone"]."',birthday='".$_GET["birthday"]."' where device_id='".$_GET["view"]."';";
+    echo $sql;
+    $ret = pg_query($db, $sql) ;
+      if(!$ret) {
+        // echo pg_last_error($db) ;
+      } else {
+         
+        // echo "Records created successfully\n";
+      }
+     
+      pg_close($db) ;   
+}*/
+//echo $_GET["view"];
+   $male =  $female = "";
+  $No =$Device_id =$Name =$Birthday =$Sex =$Heigth =$Weigth =$disease =$address = $phone = $Password = "";
+ $host        = "host=ec2-54-83-48-188.compute-1.amazonaws.com";
+      $port        = "port=5432";
+      $dbname      = "dbname=ddagopqfb1uood";
+      $credentials = "user=vsbryiqqffrttq password=7279cf8dae64f749857461db7933be4a2fb68bdc0ee6c037c158d82a755c3cf2";
+      $db = pg_connect( "$host $port $dbname $credentials"  ) ;
+      if(!$db) {
+         echo "Error : Unable to open database\n";
+      } else {
+         //echo "Opened database successfully\n";
+      }
+     $sql ="SELECT * FROM Device_information WHERE Device_id='".$_GET["view"]."';";
+    $ret = pg_query($db, $sql) ;
+      if(!$ret) {
+         echo pg_last_error($db) ;
+      } else {
+         $checking = 0;
+         while($row = pg_fetch_row($ret) ){
+        //  echo "have espname = " . $row[1] . "\n";
+          // send_LINE('PASS');
+          //  header("location: bot.php");
+           $No = $row[0];
+                      $Device_id = $row[1];
+           $Name = $row[2];
+           echo '<input type="text" id="fname" name="firstname" value="'.$Name.'">';
+           $Sex = $row[3];
+           if($Sex == 'male'){
+              $male = 'checked';
+           }else{
+              $female ='checked';
+           }
+          echo '<input id="sexfld" type="radio" value="male" name="sexfld"'.$male.' />Male <input id="sexfld" type="radio" value="female" name="sexfld" '.$female.'/> Female ';
+           $Heigth = $row[4];
+           echo'<input id="heigthfld" type="text" value="'.$Heigth.'" >';
+           $Weigth = $row[5];
+           echo'<input id="weigthfld" type="text" value="'.$Weigth.'">';
+           $disease = $row[6];
+           echo'<input id="diseasefld" type="text" value="'.$disease.'">';
+           $address  = $row[7];
+           echo'<input id="addressfld" type="text" value="'.$address.'">';
+           $phone = $row[8]; 
+          echo'<input id="phonefld" type="text" value="'.$phone.'">';
+           $Birthday = $row[9];
+           echo '<input id="birthdayfld" type="date" value="'.$Birthday.'">';
+
+         }
+         if($checking == 0){
+            // $username_err = 'No account found with that username.';
+         }
+         //echo "Records created successfully\n";
+          $sql ="SELECT * FROM device WHERE Deviceid='".$deviceid."';";
+        $ret = pg_query($db, $sql) ;
+          if(!$ret) {
+             echo pg_last_error($db) ;
+          } else {
+             $checking = 0;
+             while($row = pg_fetch_row($ret) ){
+            //  echo "have espname = " . $row[1] . "\n";
+              // send_LINE('PASS');
+              //  header("location: bot.php");
+              
+               $Password = $row[3];
+              
+             }
+             if($checking == 0){
+                // $username_err = 'No account found with that username.';
+             }
+          }
+     }
       pg_close($db) ;   
       
 }
