6314c6ba6d88689358a155d7693ee37d81847a91
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-04-19 20:31:00 +0700

    Update add_device.php

--- add_device.php
+++ add_device.php
@@ -246,7 +246,7 @@ function save_elder_info($deviceid){
       } else {
          //echo "Opened database successfully\n";
       }
-     $sql ="update Device_information set name='".$_REQUEST["name"]."',sex='".$_REQUEST["sex"]."',heigth='".$_REQUEST["heigth"]."',weigth='".$_REQUEST["weigth"]."',disease='".$_REQUEST["disease"]."' ,address='".$_REQUEST["address"]."',phone='".$_REQUEST["phone"]."',birthday='".$_REQUEST["birthday"]."' where device_id='".$deviceid."';";
+     $sql ="update Device_information set name='".$_REQUEST["name"]."',sex='".$_REQUEST["sex"]."',heigth=".$_REQUEST["heigth"].",weigth=".$_REQUEST["weigth"].",disease='".$_REQUEST["disease"]."' ,address='".$_REQUEST["address"]."',phone='".$_REQUEST["phone"]."',birthday='".$_REQUEST["birthday"]."' where device_id='".$deviceid."';";
     echo $sql;
     $ret = pg_query($db, $sql) ;
       if(!$ret) {
