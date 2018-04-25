8801e7d5318ba3c7a993f1d6694c63bd4671ea9b
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-04-26 04:07:11 +0700

    swk.

--- bot.php
+++ bot.php
@@ -44,6 +44,7 @@ if (!is_null($events['events'])) {
       send_PRESS($userId);
       send_Menu($userId);
       send_LOWBAT($userid);
+      send_FALL($userid);
        
       //send_LINE('json','Ue77a191627f6ac91899e75d92264310c');
     }
