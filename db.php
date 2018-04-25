e6c347a7de4243f053a0e5d173016d89dff546a2
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-04-26 03:44:12 +0700

    Update db.php

--- db.php
+++ db.php
@@ -189,6 +189,7 @@
          echo "Opened database successfully\n";
       }
       $sql ="SELECT * FROM userline WHERE esp='".$esp."';";
+        echo $sql;
     $ret = pg_query($db, $sql) ;
       if(!$ret) {
          echo pg_last_error($db) ;
