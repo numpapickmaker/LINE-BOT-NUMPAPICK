b3fb9509ec1cb5bc0706d44fcb53ebbc2cf689b6
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-04-26 03:47:56 +0700

    test action swk.

--- bot.php
+++ bot.php
@@ -41,9 +41,10 @@ if (!is_null($events['events'])) {
     // Reply only when message sent is in 'text' format
     if ($event['type'] == 'follow') {
       $userId = $event['source']['userId'];
-      //send_PRESS($userId);
+      send_PRESS($userId);
       send_Menu($userId);
-      
+      send_LOWBAT($userid)
+       
       //send_LINE('json','Ue77a191627f6ac91899e75d92264310c');
     }
     else if ($event['type'] == 'postback') {
