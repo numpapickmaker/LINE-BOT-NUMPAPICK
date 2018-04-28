3456475cc9d591087fecadecc14aa8d44f219b12
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-04-28 23:34:25 +0700

    Update line.php

--- line.php
+++ line.php
@@ -65,6 +65,14 @@ function send_FALL($userid){
               "imageSize"=> "cover",
               "title"=> "Menu",
               "text"=> "Please select",
+              "defaultAction"=> [
+                     "type"=> "message",
+                     "label"=> "Acknowledge",
+                     "text"=> "Acknowledge"
+  //               "type"=> "uri",
+  //               "label"=> "View detail",
+  //               "uri"=> "http://google.com"
+                ],
               "actions"=> [
                   [
                     "type"=> "message",
@@ -229,4 +237,54 @@ function send_Menu($userid){
       curl_close($ch);
       echo $result . "\r\n"; 
 }
+
+function send_Setting($userid){
+   $access_token = '3NZ4tPcC9W1t6cPI0r3ezvnsoK8KW04hbCSPxKSahSeGbeUU7lC8PQvx02uN5UyL7wOaVJ6EZ9oM5uQjkqLDNZtagQuRcS/NaaGmtopk7pBGOXtNk3lDc4KQIns5tV/jpm8yyr/114JL4uORE5czWwdB04t89/1O/w1cDnyilFU='; 
+  $messages = [
+          "type"=> "imagemap",
+          "baseUrl"=> "https://i.imgur.com/TQOKz0I.jpg",
+          "altText"=> "this is a buttons template",
+          "baseSize"=> [
+              "height"=> 1040,
+              "width"=> 1040
+          ],
+         
+                   // "uri"=> "https://numpapick.herokuapp.com/manage.php?action=".$userid
+               
+              "actions"=> [
+                      [
+                            "type"=> "uri",
+                     
+                            "thumbnailImageUrl"=> "https://i.imgur.com/TQOKz0I.jpg",
+                            "linkUri"=> "https://numpapick.herokuapp.com/main.php?action=".$userid,
+                            "area"=> [
+                                "x"=> 0,
+                                "y"=> 0,
+                                "width"=> 1040,
+                                "height"=> 1040
+                            ]
+                        ]
+                
+              
+          ]
+      ];
+      // Make a POST Request to Messaging API to reply to sender
+      $url = 'https://api.line.me/v2/bot/message/push';
+      $data = [
+        'to' => $userid,
+        'messages' => [$messages],
+      ];
+      $post = json_encode($data);
+      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      $result = curl_exec($ch);
+      curl_close($ch);
+      echo $result . "\r\n"; 
+}
+
 ?>
