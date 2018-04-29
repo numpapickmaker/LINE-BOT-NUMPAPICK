053d2bb2bfb47414f82362961bdb8cb4152f3ca7
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-04-29 19:03:26 +0700

    change layout

--- add_device1.php
+++ add_device1.php
@@ -13,14 +13,227 @@ $userid = $_GET["userid"];
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
 </head>
+<style>
+body {
+    background-color: white;
+}
+
+h1 {
+    color: #072140;
+    text-align: center;
+}
+.table {
+    border-radius: 5px;
+    width: 100%;
+    margin: 0px auto;
+    float: none;
+}
+.table td {
+   text-align: center;   
+}
+p {
+    font-family: verdana;
+    font-size: 20px;
+}
+/* Full-width input fields */
+
+.kanit {
+	font-family: 'Kanit', sans-serif;
+}.Taviraj {
+	font-family: 'Taviraj', serif;
+    font-size: 18px;
+    margin: 24px 2px;
+}
+.prevbutton {
+    background-color: white; 
+    border: none;
+    color: #072140;
+    padding: 10px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-family: 'Kanit', sans-serif;
+    font-size: 24px;
+    margin: 2px 2px;
+    cursor: pointer;
+    width: 100%;
+    border-radius: 8px;
+    border: 2px solid #072140;
+}
+.nextbutton{
+	 background-color: #072140; 
+    border: none;
+    color: white;
+    padding: 10px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-family: 'Kanit', sans-serif;
+    font-size: 24px;
+    margin: 2px 2px;
+    cursor: pointer;
+    width: 100%;
+    border-radius: 8px;
+    border: 2px solid #072140;
+}
+.addbutton{
+	 background-color: white; 
+    border: none;
+    color: #FFA400;
+    padding: 10px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-family: 'Kanit', sans-serif;
+    font-size: 24px;
+    margin: 2px 2px;
+    cursor: pointer;
+    width: 100%;
+    border-radius: 8px;
+    border: 2px solid #FFA400;
+}
+.delbutton{
+	 background-color: white; 
+    border: none;
+    color: #FF0000;
+    padding: 10px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-family: 'Kanit', sans-serif;
+    font-size: 24px;
+    margin: 2px 2px;
+    cursor: pointer;
+    width: 100%;
+    border-radius: 8px;
+    border: 2px solid #FF0000;
+}
+.Row {
+            
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: white;
+   color: black;
+   text-align: center;
+   font-size: 20px;
+    margin: 24px 2px;
+        }
+.Row.Expand {
+             height: auto;
+        }
+.step {
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbbbbb;
+  border: none;  
+  border-radius: 50%;
+  display: inline-block;
+  opacity: 0.5;
+}
+
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 0px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+/* Set a style for all buttons */
+
+
+.container {
+    padding: 16px;
+}
+
+span.psw {
+    float: right;
+    padding-top: 16px;
+}
+
+/* The Modal (background) */
+.modal {
+    display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+    padding-top: 60px;
+}
+
+/* Modal Content/Box */
+.modal-content {
+    background-color: #fefefe;
+    margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+    border: 1px solid #888;
+    width: 90%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button (x) */
+.close {
+    position: absolute;
+    right: 25px;
+    top: 0;
+    color: #000;
+    font-size: 35px;
+    font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+    color: red;
+    cursor: pointer;
+}
+
+/* Add Zoom Animation */
+.animate {
+    -webkit-animation: animatezoom 0.6s;
+    animation: animatezoom 0.6s
+}
+
+#outer
+{
+    width:100%;
+    text-align: center;
+}
+.inner
+{
+    display: inline-block;
+}
+
+.numberCircle {
+  border-radius: 50%;
+  behavior: url(PIE.htc);
+  /* remove if you don't care about IE8 */
+  width: 50px;
+  height: 50px;
+  padding: 5px;
+  background: #fff;
+  display: inline-block;
+  border: 3px solid #bbbbbb;
+  color: #bbbbbb;
+  text-align: center;
+  font: 23px kanit, sans-serif;
+  font-weight : bold
+}
+</style>
 <body>
 <p>
 <h1 class="kanit">จัดการอุปกรณ์</h1>
 <p>
 <div style="text-align: center;">
-  <span class="step"></span>
-    <span class="step" style="background-color: #000000;"></span>
-    <span class="step"></span>
+    <div class="numberCircle" style="border: 3px solid #33cc33; color: #33cc33;">1</div>
+    <div class="numberCircle" >2</div>
+    <div class="numberCircle" >3</div>
+    <div class="container" >
     <p>
     <p><button class="addbutton"  onclick="document.getElementById('id01').style.display='block'" >เพิ่ม</button></p>
     
@@ -43,14 +256,11 @@ $userid = $_GET["userid"];
     <div class="imgcontainer">
       <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
     </div>
- <div class="container Taviraj" >
- <p>
- 
-      <label for="uname"><b>อุปกรณ์</b></label>
-      <input type="text"  placeholder="เลขอุปกรณ์" name="uname" id="uname" required>
-    <p id="uname_error"></p>
-      <label for="psw"><b>รหัสผ่าน</b></label>
-      <input type="password" placeholder="รหัสผ่าน" name="psw" id="psw" required>
+<div class="container" >
+ <p><label class = "Taviraj"  for="uname">เพิ่มอุปกรณ์</label>
+      <input type="text"  placeholder="เลขอุปกรณ์" name="uname" id="uname" class = "Taviraj" required>
+      <p id="uname_error"></p>
+      <input type="password" placeholder="รหัสผ่าน" name="psw" id="psw" class = "Taviraj" required>
   
       <p id="psw_error"></p>
       <p>
@@ -58,11 +268,11 @@ $userid = $_GET["userid"];
       <p><p><p><p>
      <input type="hidden" name="userid" id="userid" value="<?php echo $userid;?>">
      
-     <button class="nextbutton"  onclick="add()" style = "font-size: 18px; width:40%" value="ยืนยัน">ยืนยัน</button>
+     <button class="nextbutton"  onclick="add()" style = "font-size: 20px; width:100%" value="ยืนยัน">ยืนยัน</button>
    
     </div>
 
-
+	</div>
    
   </div>
 </div>
