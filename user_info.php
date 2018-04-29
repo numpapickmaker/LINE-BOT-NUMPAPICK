c6337ea1e0735f30ba8416f266ef35a0db8c817d
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-04-29 18:59:06 +0700

    change date -> age

--- user_info.php
+++ user_info.php
@@ -44,7 +44,7 @@ p {
     font-size: 24px;
     margin: 2px 2px;
     cursor: pointer;
-    width: 50%;
+    width: 100%;
     border-radius: 8px;
     border: 2px solid #072140;
 }
@@ -60,7 +60,7 @@ p {
     font-size: 24px;
     margin: 2px 2px;
     cursor: pointer;
-    width: 50%;
+    width: 100%;
     border-radius: 8px;
     border: 2px solid #072140;
 }
@@ -76,7 +76,7 @@ p {
     font-size: 24px;
     margin: 2px 2px;
     cursor: pointer;
-    width: 50%;
+    width: 100%;
     border-radius: 8px;
     border: 2px solid #FFA400;
 }
@@ -107,11 +107,12 @@ p {
 input[type=text], select {
     width: 100%;
     padding: 12px 28px;
-    margin: 8px 2px;
+    margin: 0px 2px;
     display: inline-block;
     border: 1px solid #ccc;
     border-radius: 4px;
     box-sizing: border-box;
+    
 }
 /* Set a style for all buttons */
 .container {
@@ -156,11 +157,39 @@ span.psw {
     color: red;
     cursor: pointer;
 }
+
 /* Add Zoom Animation */
 .animate {
     -webkit-animation: animatezoom 0.6s;
     animation: animatezoom 0.6s
 }
+
+#outer
+{
+    width:100%;
+    text-align: center;
+}
+.inner
+{
+    display: inline-block;
+}
+
+.numberCircle {
+  border-radius: 50%;
+  behavior: url(PIE.htc);
+  /* remove if you don't care about IE8 */
+  width: 50px;
+  height: 50px;
+  padding: 5px;
+  background: #fff;
+  display: inline-block;
+  border: 3px solid #bbbbbb;
+  color: #bbbbbb;
+  text-align: center;
+  font: 23px kanit, sans-serif;
+  font-weight : bold
+}
+
 </style>
 </head>
 <body>
@@ -168,16 +197,16 @@ span.psw {
 
 <p>
     <div style="text-align: center;">
-    <span class="step" style="background-color: #000000;"></span>
-    <span class="step" ></span>
-    <span class="step"></span>
+    <div class="numberCircle" >1</div>
+    <div class="numberCircle" >2</div>
+    <div class="numberCircle" >3</div>
     <p>
     </div>
 <form name="elder_form" id="elder_form" onSubmit="return user()">
-    
+    <div class="container" >
     <label  class = "Taviraj" >กรอกประวัติผู้ดูแล</label>
     
-    <input type="text"  name="fname" id="fname" placeholder="ชื่อ" class = "Taviraj"  required >
+    <input  type="text"  name="fname" id="fname" placeholder="ชื่อ" class = "Taviraj"  required >
     <p id="fname_error"></p>
     <input type="text" id="lname" name="lastname" placeholder="นามสกุล" class = "Taviraj"  required>
     <p id="lname_error"></p>
@@ -187,15 +216,14 @@ span.psw {
     <p id="email_error"></p>
     <input type="text" id="career" name="career" placeholder="อาชีพ" class = "Taviraj" required >
     <p id="career_error"></p>
-    <p id="test" class="Taviraj">วัดเกิด</p> 
-    <input type="Date" id="birthday" name="birthday"  class = "Taviraj" required> 
+    <input type="text" id="birthday" name="birthday" placeholder="อายุ" class = "Taviraj" required> 
   <p id="birthday_error"></p>
   <div style="text-align: center;">
     <input type="hidden" name="userid" id="userid" value="<?php echo $userid;?>" >
     
     <button class="nextbutton" type="submit" form="elder_form">ยืนยัน</button></p>
-     
-  </div>
+    
+    </div>
    </form>
 <div class="footer">
   <p>   </p>
@@ -206,20 +234,18 @@ function user(){
     var fname = document.getElementById("fname").value;
     var lname = document.getElementById("lname").value;
     var phone = document.getElementById("phone").value;
-  var email = document.getElementById("email").value;
+  	var email = document.getElementById("email").value;
     var career = document.getElementById("career").value;
     var birthday = document.getElementById("birthday").value;
     var userid = document.getElementById("userid").value;
     
         var xmlhttp = new XMLHttpRequest();
-
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
               // document.getElementById("birthday_error").innerHTML = this.responseText;
                 if(this.responseText == "success"){
                   window.location.replace("https://numpapick.herokuapp.com/add_device1.php?userid="+userid);
                 }
-
             }
         };
         xmlhttp.open("GET", "https://numpapick.herokuapp.com/add_device.php?info=1&fname=" + fname + "&lname="+lname+"&phone="+phone+"&email="+email+"&career="+career+"&birthday="+birthday+"&userid="+userid, true);
