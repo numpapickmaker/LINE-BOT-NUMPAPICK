dfa2360b5f5ecfd734ca00f7755691821f73b4d0
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-05-04 03:01:22 +0700

    update วัน / เดือน / ปี / 2

--- user_info.php
+++ user_info.php
@@ -213,8 +213,8 @@ span.psw {
     <input type="text" id="career" name="career" placeholder="อาชีพ" class = "Taviraj" required >
     
     <p id="career_error"></p>
-    
-     <select class = "Taviraj" id="dd" name="dd" style="width: 25%" required>
+      <label  class = "Taviraj" >วัน / เดือน / ปี</label>
+     <select class = "Taviraj" id="dd" name="dd" style="width: 100% padding: 12px 28px; margin: 0px 2px;" required>
      <option value="00">วัน</option>
       <option value="01">01</option>
       <option value="02">02</option>
@@ -250,7 +250,8 @@ span.psw {
       <option value="31">31</option>
       
 	</select>
-    <select class = "Taviraj" id="mm" name="mm" style="width: 25%" required>
+  <p></p>
+    <select class = "Taviraj" id="mm" name="mm" style="width: 100% padding: 12px 28px; margin: 0px 2px;" required>
       <option value="00">เดือน</option>
       <option value="01">01</option>
       <option value="02">02</option>
@@ -265,7 +266,8 @@ span.psw {
       <option value="11">11</option>
       <option value="12">12</option>
 	</select>
-    <select class = "Taviraj" id="yy" name="yy" style="width: 45%" required>
+  <p></p>
+    <select class = "Taviraj" id="yy" name="yy" style="width: 100% padding: 12px 28px; margin: 0px 2px;" required>
       <option value="00">ปี</option>
       <option value="2018">2018</option>
       <option value="2017">2017</option>
@@ -366,7 +368,7 @@ span.psw {
       <option value="1930">1930</option>
       
 	</select>
-    
+  <p></p>  
   
   <div style="text-align: center;">
     <input type="hidden" name="userid" id="userid" value="<?php echo $userid;?>" >
