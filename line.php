590df25007d9b6732e6d03ef1ea02f75ab9ba5fa
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-05-06 01:54:40 +0700

    Update line.php

--- line.php
+++ line.php
@@ -333,12 +333,12 @@ function send_CALL($userid){
 function send_tutorial($userid){
    $access_token = '3NZ4tPcC9W1t6cPI0r3ezvnsoK8KW04hbCSPxKSahSeGbeUU7lC8PQvx02uN5UyL7wOaVJ6EZ9oM5uQjkqLDNZtagQuRcS/NaaGmtopk7pBGOXtNk3lDc4KQIns5tV/jpm8yyr/114JL4uORE5czWwdB04t89/1O/w1cDnyilFU='; 
   $messages = [
-          "type"=> "image",
-          "originalContentUrl": "https://i.imgur.com/GcU2NS9.jpg",
-    "previewImageUrl": "https://i.imgur.com/GcU2NS9.jpg"
-         
-                   // "uri"=> "https://numpapick.herokuapp.com/manage.php?action=".$userid
-
+              
+          
+                "type"=> "image",
+                "originalContentUrl"=> "https://i.imgur.com/GcU2NS9.jpg",
+                "previewImageUrl"=> "https://i.imgur.com/GcU2NS9.jpg"
+            
       ];
       // Make a POST Request to Messaging API to reply to sender
       $url = 'https://api.line.me/v2/bot/message/push';
