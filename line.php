43e56dfe4a05af47860d706779dd31d5ba3a28d5
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-05-06 01:40:52 +0700

    suk update  send_tutorial

--- line.php
+++ line.php
@@ -330,4 +330,36 @@ function send_CALL($userid){
       curl_close($ch);
       echo $result . "\r\n"; 
 }
+function send_tutorial($userid){
+   $access_token = '3NZ4tPcC9W1t6cPI0r3ezvnsoK8KW04hbCSPxKSahSeGbeUU7lC8PQvx02uN5UyL7wOaVJ6EZ9oM5uQjkqLDNZtagQuRcS/NaaGmtopk7pBGOXtNk3lDc4KQIns5tV/jpm8yyr/114JL4uORE5czWwdB04t89/1O/w1cDnyilFU='; 
+  $messages = [
+          "type"=> "imagemap",
+          "baseUrl"=> "https://i.imgur.com/GcU2NS9.jpg",
+          "altText"=> "this is a buttons template",
+          "baseSize"=> [
+              "width"=> 1040,
+              "height"=> 900
+          ],
+         
+                   // "uri"=> "https://numpapick.herokuapp.com/manage.php?action=".$userid
+
+      ];
+      // Make a POST Request to Messaging API to reply to sender
+      $url = 'https://api.line.me/v2/bot/message/push';
+      $data = [
+        'to' => $userid,
+        'messages' => [$messages],
+      ];
+      $post = json_encode($data);
+      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      $result = curl_exec($ch);
+      curl_close($ch);
+      echo $result . "\r\n"; 
+}
 ?>
