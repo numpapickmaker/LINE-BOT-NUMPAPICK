381e4057b30fc737139fafc172bc60cec39f4bf2
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-05-06 01:51:43 +0700

    immage

--- line.php
+++ line.php
@@ -333,13 +333,9 @@ function send_CALL($userid){
 function send_tutorial($userid){
    $access_token = '3NZ4tPcC9W1t6cPI0r3ezvnsoK8KW04hbCSPxKSahSeGbeUU7lC8PQvx02uN5UyL7wOaVJ6EZ9oM5uQjkqLDNZtagQuRcS/NaaGmtopk7pBGOXtNk3lDc4KQIns5tV/jpm8yyr/114JL4uORE5czWwdB04t89/1O/w1cDnyilFU='; 
   $messages = [
-          "type"=> "imagemap",
-          "baseUrl"=> "https://i.imgur.com/GcU2NS9.jpg",
-          "altText"=> "this is a buttons template",
-          "baseSize"=> [
-              "width"=> 1040,
-              "height"=> 900
-          ],
+          "type"=> "image",
+          "originalContentUrl": "https://i.imgur.com/GcU2NS9.jpg",
+    "previewImageUrl": "https://i.imgur.com/GcU2NS9.jpg"
          
                    // "uri"=> "https://numpapick.herokuapp.com/manage.php?action=".$userid
 
