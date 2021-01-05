9cbed661fa2811e09ddaac55a17a7ddb5e670146
numpapickmaker <numpapickmaker@users.noreply.github.com>
2021-01-05 23:27:56 +0700

    Update line.php

--- line.php
+++ line.php
@@ -337,8 +337,7 @@ function send_tutorial($userid){
           
                 "type"=> "images",
                 "originalContentUrl"=> "https://i.imgur.com/GcU2NS9.jpg",
-//                 "previewImageUrl"=> "https://i.imgur.com/GcU2NS9.jpg"
-                "previewImageUrl"=> "https://imgur.com/PWpbvVW.jpg"
+                "previewImageUrl"=> "https://i.imgur.com/GcU2NS9.jpg"
     
             
       ];
@@ -360,4 +359,33 @@ function send_tutorial($userid){
       curl_close($ch);
       echo $result . "\r\n"; 
 }
+
+function send_image_test_api($userid){
+    $access_token = '3NZ4tPcC9W1t6cPI0r3ezvnsoK8KW04hbCSPxKSahSeGbeUU7lC8PQvx02uN5UyL7wOaVJ6EZ9oM5uQjkqLDNZtagQuRcS/NaaGmtopk7pBGOXtNk3lDc4KQIns5tV/jpm8yyr/114JL4uORE5czWwdB04t89/1O/w1cDnyilFU='; 
+    $messages = [
+        "type"=> "images",
+        "originalContentUrl"=> "https://imgur.com/PWpbvVW.jpg",
+        "previewImageUrl"=> "https://imgur.com/PWpbvVW.jpg"
+    ];
+    // Make a POST Request to Messaging API to reply to sender
+    $url = 'https://api.line.me/v2/bot/message/push';
+    $data = [
+        'to' => $userid,
+        'messages' => [$messages],
+    ];
+    $post = json_encode($data);
+    $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    echo $result . "\r\n"; 
+}
+
+
+
 ?>
